b53c343e8df45b3f18dc0a50c5d57846005e279e
Martin Owens <doctormo@gmail.com>
2018-05-20 05:44:32 -0400

    Clean coverage reporter

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,7 @@
 
-python3-coverage run --source Barcode,inkex setup.py test &> /dev/null
-python2.7-coverage run -a --source Barcode,inkex setup.py test &> /dev/null
+export MODULES='inkex'
+
+python3-coverage run --source $MODULES setup.py test &> /dev/null
+#python2.7-coverage run -a --source $MODULES setup.py test &> /dev/null
 python3-coverage report -m
 

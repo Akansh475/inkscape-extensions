7798996320273c864053992524a3c900a9ea4318
Martin Owens <doctormo@gmail.com>
2018-09-10 11:18:27 +0200

    Remove executable bit from python depricated modules

--- inkex/deprecated-simple/bezmisc.py
+++ inkex/deprecated-simple/bezmisc.py
@@ -1,54 +1,36 @@
-# COPYRIGHT
-"""DOCSTRING"""
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# pylint: disable=invalid-name
+"""Depricated bezmisc API"""
 
-import math, cmath
+from inkex.depricated import depricate
+from inkex import bezier
 
-def rootWrapper(a,b,c,d):
-    """Unknown"""
-
-def bezierparameterize(((bx0,by0),(bx1,by1),(bx2,by2),(bx3,by3))):
-    """parametric bezier"""
-
-def linebezierintersect(((lx1,ly1),(lx2,ly2)),((bx0,by0),(bx1,by1),(bx2,by2),(bx3,by3))):
-    """parametric line"""
-
-def bezierpointatt(((bx0,by0),(bx1,by1),(bx2,by2),(bx3,by3)),t):
-    """something"""
-
-def bezierslopeatt(((bx0,by0),(bx1,by1),(bx2,by2),(bx3,by3)),t):
-    """something"""
-
-def beziertatslope(((bx0,by0),(bx1,by1),(bx2,by2),(bx3,by3)),(dy,dx)):
-    """Unknown"""
-
-def tpoint((x1,y1),(x2,y2),t):
-    """Unknown"""
-
-def beziersplitatt(((bx0,by0),(bx1,by1),(bx2,by2),(bx3,by3)),t):
-    """Unknown"""
-
-def pointdistance((x1,y1),(x2,y2)):
-    """Uknown"""
-
-def Gravesen_addifclose(b, len, error = 0.001):
-    """Unknown"""
-
-def bezierlengthGravesen(b, error = 0.001):
-    """Unknown"""
-
-def balf(t):
-    """Unknown"""
-    retval = (balfax*(t**2) + balfbx*t + balfcx)**2 + (balfay*(t**2) + balfby*t + balfcy)**2
-    return math.sqrt(retval)
-
-def Simpson(f, a, b, n_limit, tolerance):
-    """Unknown"""
-
-def bezierlengthSimpson(((bx0,by0),(bx1,by1),(bx2,by2),(bx3,by3)), tolerance = 0.001):
-    """Unknown"""
-
-def beziertatlength(((bx0,by0),(bx1,by1),(bx2,by2),(bx3,by3)), l = 0.5, tolerance = 0.001):
-    """Unknown"""
-
-#default bezier length method
+bezierparameterize = depricate(bezier.bezierparameterize)
+linebezierintersect = depricate(bezier.linebezierintersect)
+bezierpointatt = depricate(bezier.bezierpointatt)
+bezierslopeatt = depricate(bezier.bezierslopeatt)
+beziertatslope = depricate(bezier.beziertatslope)
+tpoint = depricate(bezier.tpoint)
+beziersplitatt = depricate(bezier.beziersplitatt)
+pointdistance = depricate(bezier.pointdistance)
+Gravesen_addifclose = depricate(bezier.Gravesen_addifclose)
+bezierlengthGravesen = depricate(bezier.bezierlengthGravesen)
+balf = depricate(bezier.balf)
+Simpson = depricate(bezier.Simpson)
+bezierlengthSimpson = depricate(bezier.bezierlengthSimpson)
+beziertatlength = depricate(bezier.beziertatlength)
 bezierlength = bezierlengthSimpson

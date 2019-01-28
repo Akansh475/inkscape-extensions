1f34ec85a7ce26fa5cfce89d5cea22c50df1bcf8
Martin Owens <doctormo@gmail.com>
2019-01-28 03:12:19 -0500

    Fix svg calendar tests

--- svgcalendar.py
+++ svgcalendar.py
@@ -157,8 +157,8 @@ class SVGCalendar(inkex.Effect):
         else:
             calendar.setfirstweekday(0)
         # Convert string numbers with unit to user space float numbers
-        self.options.month_width  = self.unittouu( self.options.month_width )
-        self.options.month_margin = self.unittouu( self.options.month_margin )
+        self.options.month_width = self.svg.unittouu(self.options.month_width)
+        self.options.month_margin = self.svg.unittouu(self.options.month_margin)
 
     # initial values
     month_x_pos = 0
@@ -167,8 +167,8 @@ class SVGCalendar(inkex.Effect):
 
     def calculate_size_and_positions(self):
         #month_margin month_width months_per_line auto_organize
-        self.doc_w = self.unittouu(self.document.getroot().get('width'))
-        self.doc_h = self.unittouu(self.document.getroot().get('height'))
+        self.doc_w = self.svg.unittouu(self.document.getroot().get('width'))
+        self.doc_h = self.svg.unittouu(self.document.getroot().get('height'))
         if self.options.show_weeknr:
             self.cols_before = 1
         else:
--- tests/test_svgcalendar.py
+++ tests/test_svgcalendar.py
@@ -1,107 +1,92 @@
-#!/usr/bin/env python
-
+"""
+All tests for the svg calendar extension
+"""
 import calendar
+from datetime import datetime
 
 from tests.base import TestCase
-import unittest
-from svgcalendar import *
+
+from svgcalendar import SVGCalendar
 
 class CalendarArguments(TestCase):
+    """Test arguments to calendar extensions"""
+    ext_model = SVGCalendar
+
     def test_default_names_list(self):
-        args = [self.data_file('svg', 'minimal-blank.svg')]
-        e = SVGCalendar()
-        e.run(args)
-        self.assertEqual( e.options.month_names[0], 'January' )
-        self.assertEqual( e.options.month_names[11], 'December' )
-        self.assertEqual( e.options.day_names[0], 'Sun' )
-        self.assertEqual( e.options.day_names[6], 'Sat' )
+        """Test default names"""
+        effect = self.assertEffect()
+        self.assertEqual(effect.options.month_names[0], 'January')
+        self.assertEqual(effect.options.month_names[11], 'December')
+        self.assertEqual(effect.options.day_names[0], 'Sun')
+        self.assertEqual(effect.options.day_names[6], 'Sat')
+        self.assertEqual(effect.options.year, datetime.today().year)
+        self.assertEqual(calendar.firstweekday(), 6)
 
     def test_modifyed_names_list(self):
-        args = [
+        """Test modified names list"""
+        effect = self.assertEffect(args=[
             '--month-names=JAN FEV MAR ABR MAI JUN JUL AGO SET OUT NOV DEZ',
             '--day-names=DOM SEG TER QUA QUI SEX SAB',
-            self.data_file('svg', 'minimal-blank.svg'),
-            ]
-        e = SVGCalendar()
-        e.run(args)
-        self.assertEqual( e.options.month_names[0], 'JAN' )
-        self.assertEqual( e.options.month_names[11], 'DEZ' )
-        self.assertEqual( e.options.day_names[0], 'DOM' )
-        self.assertEqual( e.options.day_names[6], 'SAB' )
+        ])
+        self.assertEqual(effect.options.month_names[0], 'JAN')
+        self.assertEqual(effect.options.month_names[11], 'DEZ')
+        self.assertEqual(effect.options.day_names[0], 'DOM')
+        self.assertEqual(effect.options.day_names[6], 'SAB')
 
-    def test_starting_or_ending_spaces_must_not_affect_names_list(self):
-        args = [
+    def test_starting_names_list(self):
+        """Starting or ending spaces must not affect names"""
+        effect = self.assertEffect(args=[
             '--month-names= JAN FEV MAR ABR MAI JUN JUL AGO SET OUT NOV DEZ ',
             '--day-names=    DOM SEG TER QUA QUI SEX SAB    ',
-            self.data_file('svg', 'minimal-blank.svg'),
-            ]
-        e = SVGCalendar()
-        e.run(args)
-        self.assertEqual( e.options.month_names[0], 'JAN' )
-        self.assertEqual( e.options.month_names[11], 'DEZ' )
-        self.assertEqual( e.options.day_names[0], 'DOM' )
-        self.assertEqual( e.options.day_names[6], 'SAB' )
+        ])
+        self.assertEqual(effect.options.month_names[0], 'JAN')
+        self.assertEqual(effect.options.month_names[11], 'DEZ')
+        self.assertEqual(effect.options.day_names[0], 'DOM')
+        self.assertEqual(effect.options.day_names[6], 'SAB')
 
-    def test_inner_extra_spaces_must_not_affect_names_list(self):
-        args = [
+    def test_inner_extra_spaces(self):
+        """Extra spaces must not affect names"""
+        effect = self.assertEffect(args=[
             '--month-names=JAN FEV        MAR ABR MAI JUN JUL AGO SET OUT NOV DEZ',
             '--day-names=DOM SEG        TER QUA QUI SEX SAB',
-            self.data_file('svg', 'minimal-blank.svg'),
-            ]
-        e = SVGCalendar()
-        e.run(args)
-        self.assertEqual( e.options.month_names[0], 'JAN' )
-        self.assertEqual( e.options.month_names[2], 'MAR' )
-        self.assertEqual( e.options.month_names[11], 'DEZ' )
-        self.assertEqual( e.options.day_names[0], 'DOM' )
-        self.assertEqual( e.options.day_names[2], 'TER' )
-        self.assertEqual( e.options.day_names[6], 'SAB' )
+        ])
+        self.assertEqual(effect.options.month_names[0], 'JAN')
+        self.assertEqual(effect.options.month_names[2], 'MAR')
+        self.assertEqual(effect.options.month_names[11], 'DEZ')
+        self.assertEqual(effect.options.day_names[0], 'DOM')
+        self.assertEqual(effect.options.day_names[2], 'TER')
+        self.assertEqual(effect.options.day_names[6], 'SAB')
 
-    def test_default_year_must_be_the_current_year(self):
-        args = [self.data_file('svg', 'minimal-blank.svg')]
-        e = SVGCalendar()
-        e.run(args)
-        self.assertEqual( e.options.year, datetime.today().year )
+    def test_converted_year_zero(self):
+        """Year equal to 0 is converted to currect year"""
+        effect = self.assertEffect(args=['--year=0'])
+        self.assertEqual(effect.options.year, datetime.today().year)
 
-    def test_option_year_equal_0_is_converted_to_current_year(self):
-        args = ['--year=0', self.data_file('svg', 'minimal-blank.svg')]
-        e = SVGCalendar()
-        e.run(args)
-        self.assertEqual( e.options.year, datetime.today().year )
-
-    def test_option_year_2000_configuration(self):
-        args = ['--year=2000', self.data_file('svg', 'minimal-blank.svg')]
-        e = SVGCalendar()
-        e.run(args)
-        self.assertEqual( e.options.year, 2000 )
-
-    def test_default_week_start_day(self):
-        args = [self.data_file('svg', 'minimal-blank.svg')]
-        e = SVGCalendar()
-        e.run(args)
-        self.assertEqual( calendar.firstweekday(), 6 )
+    def test_converted_year_thousand(self):
+        """Year equal to 2000 configuration"""
+        effect = self.assertEffect(args=['--year=2000'])
+        self.assertEqual(effect.options.year, 2000)
 
     def test_configuring_week_start_sun(self):
-        args = ['--start-day=sun', self.data_file('svg', 'minimal-blank.svg')]
-        e = SVGCalendar()
-        e.run(args)
-        self.assertEqual( calendar.firstweekday(), 6 )
+        """Week start is set to Sunday"""
+        self.assertEffect(args=['--start-day=sun'])
+        self.assertEqual(calendar.firstweekday(), 6)
 
     def test_configuring_week_start_mon(self):
-        args = ['--start-day=mon', self.data_file('svg', 'minimal-blank.svg')]
-        e = SVGCalendar()
-        e.run(args)
-        self.assertEqual( calendar.firstweekday(), 0 )
+        """Week start is set to Monday"""
+        self.assertEffect(args=['--start-day=mon'])
+        self.assertEqual(calendar.firstweekday(), 0)
 
 
 class CalendarMethods(TestCase):
-    def test_recognize_a_weekend(self):
-        args = ['--start-day=sun', '--weekend=sat+sun',    self.data_file('svg', 'minimal-blank.svg')]
-        e = SVGCalendar()
-        e.run(args)
-        self.assertTrue(    e.is_weekend(0), 'Sunday is weekend in this configuration' )
-        self.assertTrue(    e.is_weekend(6), 'Saturday is weekend in this configuration' )
-        self.assertFalse( e.is_weekend(1), 'Monday is NOT weekend' )
+    """Test calendar methods"""
+    ext_model = SVGCalendar
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_recognize_a_weekend(self):
+        """Recognise a weekend"""
+        effect = self.assertEffect(args=[
+            '--start-day=sun', '--weekend=sat+sun',
+        ])
+        self.assertTrue(effect.is_weekend(0), 'Sunday is weekend in this configuration')
+        self.assertTrue(effect.is_weekend(6), 'Saturday is weekend in this configuration')
+        self.assertFalse(effect.is_weekend(1), 'Monday is NOT weekend')

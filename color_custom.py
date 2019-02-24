eb855eb366d02e5d9792c238793ad4f77ff679e3
Ryan Jarvis <ryan@shopboxretail.com>
2019-02-24 10:19:27 -0800

    Rewrite color_custom to not use eval().  Include tests.

--- color_custom.py
+++ color_custom.py
@@ -2,29 +2,67 @@
 # coding=utf-8
 from __future__ import absolute_import, division
 
+import ast
+import operator as op
+
 import coloreffect
+
+# supported operators
+operators = {ast.Add: op.add,
+             ast.Sub: op.sub,
+             ast.Mult: op.mul,
+             ast.Div: op.truediv,
+             ast.Pow: op.pow,
+             ast.BitXor: op.xor,
+             ast.USub: op.neg}
+
+
+def eval_expr(expr, namespace):
+    """
+    >>> eval_expr('2^6')
+    4
+    >>> eval_expr('2**6')
+    64
+    >>> eval_expr('1 + 2*3**(4^5) / (6 + -7)')
+    -5.0
+    """
+    return eval_(ast.parse(expr, mode='eval').body, namespace)
+
+
+def eval_(node, namespace):
+    if isinstance(node, ast.Num):  # <number>
+        return node.n
+    elif isinstance(node, ast.Name):  # <variable> (must be in namespace)
+        return namespace[node.id]
+    elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
+        return operators[type(node.op)](eval_(node.left, namespace), eval_(node.right, namespace))
+    elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+        return operators[type(node.op)](eval_(node.operand, namespace))
+    else:
+        raise TypeError(node)
 
 
 class C(coloreffect.ColorEffect):
     def __init__(self):
         coloreffect.ColorEffect.__init__(self)
-        self.arg_parser.add_argument("--r",
+        self.arg_parser.add_argument("-r", "--r",
                                      dest="rFunction", default="r",
                                      help="red channel function")
-        self.arg_parser.add_argument("--g",
+        self.arg_parser.add_argument("-g", "--g",
                                      dest="gFunction", default="g",
                                      help="green channel function")
-        self.arg_parser.add_argument("--b",
+        self.arg_parser.add_argument("-b", "--b",
                                      dest="bFunction", default="b",
                                      help="blue channel function")
-        self.arg_parser.add_argument("--tab",
+        self.arg_parser.add_argument("-t", "--tab",
                                      help="The selected UI-tab when OK was pressed")
-        self.arg_parser.add_argument("--scale", type=float, default=1.0,
+        self.arg_parser.add_argument("-s", "--scale",
+                                     type=float, default=1,
                                      help="The input (r,g,b) range")
 
     def normalize(self, v):
         if v < 0:
-            return 0.0
+            return 0
         if v > self.options.scale:
             return self.options.scale
         return v
@@ -39,14 +77,11 @@ class C(coloreffect.ColorEffect):
         b = _b / factor
 
         # add stuff to be accessible from within the custom color function here.
-        safeenv = {'__builtins__': {}, 'r': r, 'g': g, 'b': b}
+        safe_namespace = {'r': r, 'g': g, 'b': b}
 
-        try:
-            r2 = self.normalize(eval(self.options.rFunction, safeenv))
-            g2 = self.normalize(eval(self.options.gFunction, safeenv))
-            b2 = self.normalize(eval(self.options.bFunction, safeenv))
-        except:
-            return self._hexstr(255.0, 0.0, 0.0)
+        r2 = self.normalize(eval_expr(self.options.rFunction.strip(), safe_namespace))
+        g2 = self.normalize(eval_expr(self.options.gFunction.strip(), safe_namespace))
+        b2 = self.normalize(eval_expr(self.options.bFunction.strip(), safe_namespace))
         return self._hexstr(r2 * factor, g2 * factor, b2 * factor)
 
 
---
+++ tests/test_color_custom.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from color_custom import C
+from tests.base import TestCase
+
+
+class ColorCustomBasicTest(TestCase):
+    def setUp(self):
+        self.e = C()
+
+    def test_default_values(self):
+        """ The default ranges are set to 0, and thus the color should not change. """
+        args = [self.empty_svg]
+        self.e.run(args)
+        col = self.e.colmod(255, 255, 255)
+        self.assertEqual("ffffff", col)
+
+    def test_double_red(self):
+        args = ['-r r*2', self.empty_svg]
+        self.e.run(args)
+        col = self.e.colmod(100, 0, 0)
+        self.assertEqual("c80000", col)
+
+    def test_switch_green_blue(self):
+        args = ['-g b', '-b g', self.empty_svg]
+        self.e.run(args)
+        col = self.e.colmod(12, 34, 56)
+        self.assertEqual("0c3822", col)
+
+    def test_switch_green_blue_and_double_red(self):
+        args = ['-g b', '-b g', '-r r*2', self.empty_svg]
+        self.e.run(args)
+        col = self.e.colmod(12, 34, 56)
+        self.assertEqual("183822", col)
+
+    def test_set_red_to_16_scale255(self):
+        args = ['-s 255', '-r 16', self.empty_svg]
+        self.e.run(args)
+        col = self.e.colmod(0, 0, 0)
+        self.assertEqual("100000", col)
+
+    def test_set_red_to_16_scale1(self):
+        args = ['-s 1', '-r 0.0625', self.empty_svg]
+        self.e.run(args)
+        col = self.e.colmod(0, 0, 0)
+        self.assertEqual("100000", col)
+
+    def test_set_red_to_400(self):
+        """This should cap red at 255"""
+        args = ['-r 400', self.empty_svg]
+        self.e.run(args)
+        col = self.e.colmod(0, 0, 0)
+        self.assertEqual("ff0000", col)
+
+    def test_set_red_to_negative(self):
+        """This should cap red at 0"""
+        args = ['-r -400', self.empty_svg]
+        self.e.run(args)
+        col = self.e.colmod(0, 0, 0)
+        self.assertEqual("000000", col)
+
+    def test_evil_fails(self):
+        """
+        eval shouldn't allow for evil things to happen
+
+        Here we try and check if a file exists but it could just as easily
+        overwrite or delete the file
+
+        """
+
+        args = ["-r __import__('os').path.exists('__init__.py')", self.empty_svg]
+        self.e.run(args)
+
+        with self.assertRaises(TypeError):
+            self.e.colmod(0, 0, 0)
+
+    def test_invalid_operator(self):
+        args = ["-r r % 100", self.empty_svg]
+        self.e.run(args)
+
+        with self.assertRaises(KeyError):
+            self.e.colmod(0, 0, 0)
+
+    def test_bad_syntax(self):
+        args = ["-r r + 100)", self.empty_svg]
+        self.e.run(args)
+
+        with self.assertRaises(SyntaxError):
+            self.e.colmod(0, 0, 0)

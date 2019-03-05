55594f3268bf9ed4ddef4db2a2a96cc45fb8a220
Ryan Jarvis <ryan@shopboxretail.com>
2019-03-05 15:55:02 -0800

    Fix Scour test path

--- tests/test_scour.py
+++ tests/test_scour.py
@@ -1,6 +1,11 @@
 # coding=utf-8
+import os
+
 from output_scour import ScourInkscape
 from tests.base import InkscapeExtensionTestMixin, TestCase
+
+# The current files directory
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 class ScourBasicTests(InkscapeExtensionTestMixin, TestCase):
@@ -9,9 +14,9 @@ class ScourBasicTests(InkscapeExtensionTestMixin, TestCase):
         self.e = self.effect()
 
     def test_working(self):
-        input = "tests/data/svg/default-inkscape-SVG.svg"
+        input = os.path.join(dir_path, "data/svg/default-inkscape-SVG.svg")
         output = self.temp_file(suffix='.svg')
-        output_expected = "tests/data/svg/default-inkscape-SVG_scoured.svg"
+        output_expected = os.path.join(dir_path, "data/svg/default-inkscape-SVG_scoured.svg")
 
         self.e.run(['--output', output, input])
 

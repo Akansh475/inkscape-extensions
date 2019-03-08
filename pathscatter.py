0870c337644e56ecc68f0520f37896ebce996063
Ryan Jarvis <ryan@shopboxretail.com>
2019-03-07 17:10:08 -0800

    PEP8 pathscatter.py

--- pathscatter.py
+++ pathscatter.py
@@ -31,39 +31,42 @@ as painted on these lines.
 Now move and bend L to make it fit a skeleton, and see what happens to the normals:
 they move and rotate, deforming the pattern.
 """
-# standard library
 import copy
 import random
-# third party
+
 from lxml import etree
-# local library
-import pathmodifier
 
 import inkex
+import pathmodifier
 from inkex.localize import _
 from inkex.utils import inkbool
+import simpletransform
+
 
 def flipxy(path):
     for pathcomp in path:
         for ctl in pathcomp:
             for pt in ctl:
-                tmp=pt[0]
-                pt[0]=-pt[1]
-                pt[1]=-tmp
+                tmp = pt[0]
+                pt[0] = -pt[1]
+                pt[1] = -tmp
 
-def offset(pathcomp,dx,dy):
+
+def offset(pathcomp, dx, dy):
     for ctl in pathcomp:
         for pt in ctl:
-            pt[0]+=dx
-            pt[1]+=dy
+            pt[0] += dx
+            pt[1] += dy
 
-def stretch(pathcomp,xscale,yscale,org):
+
+def stretch(pathcomp, xscale, yscale, org):
     for ctl in pathcomp:
         for pt in ctl:
-            pt[0]=org[0]+(pt[0]-org[0])*xscale
-            pt[1]=org[1]+(pt[1]-org[1])*yscale
+            pt[0] = org[0] + (pt[0] - org[0]) * xscale
+            pt[1] = org[1] + (pt[1] - org[1]) * yscale
 
-def linearize(p,tolerance=0.001):
+
+def linearize(p, tolerance=0.001):
     """
     This function receives a component of a 'cubicsuperpath' and returns two things:
     The path subdivided in many straight segments, and an array containing the length of each segment.
@@ -71,28 +74,29 @@ def linearize(p,tolerance=0.001):
     We could work with bezier path as well, but bezier arc lengths are (re)computed for each point
     in the deformed object. For complex paths, this might take a while.
     """
-    zero=0.000001
-    i=0
-    d=0
-    lengths=[]
-    while i<len(p)-1:
-        box  = inkex.pointdistance(p[i  ][1],p[i  ][2])
-        box += inkex.pointdistance(p[i  ][2],p[i+1][0])
-        box += inkex.pointdistance(p[i+1][0],p[i+1][1])
-        chord = inkex.pointdistance(p[i][1], p[i+1][1])
+    zero = 0.000001
+    i = 0
+    d = 0
+    lengths = []
+    while i < len(p) - 1:
+        box = inkex.pointdistance(p[i][1], p[i][2])
+        box += inkex.pointdistance(p[i][2], p[i + 1][0])
+        box += inkex.pointdistance(p[i + 1][0], p[i + 1][1])
+        chord = inkex.pointdistance(p[i][1], p[i + 1][1])
         if (box - chord) > tolerance:
-            b1, b2 = inkex.beziersplitatt([p[i][1],p[i][2],p[i+1][0],p[i+1][1]], 0.5)
-            p[i  ][2][0],p[i  ][2][1]=b1[1]
-            p[i+1][0][0],p[i+1][0][1]=b2[2]
-            p.insert(i+1,[[b1[2][0],b1[2][1]],[b1[3][0],b1[3][1]],[b2[1][0],b2[1][1]]])
+            b1, b2 = inkex.beziersplitatt([p[i][1], p[i][2], p[i + 1][0], p[i + 1][1]], 0.5)
+            p[i][2][0], p[i][2][1] = b1[1]
+            p[i + 1][0][0], p[i + 1][0][1] = b2[2]
+            p.insert(i + 1, [[b1[2][0], b1[2][1]], [b1[3][0], b1[3][1]], [b2[1][0], b2[1][1]]])
         else:
-            d=(box+chord)/2
+            d = (box + chord) / 2
             lengths.append(d)
-            i+=1
-    new=[p[i][1] for i in range(0,len(p)-1) if lengths[i]>zero]
+            i += 1
+    new = [p[i][1] for i in range(0, len(p) - 1) if lengths[i] > zero]
     new.append(p[-1][1])
-    lengths=[l for l in lengths if l>zero]
-    return(new,lengths)
+    lengths = [l for l in lengths if l > zero]
+    return (new, lengths)
+
 
 class PathScatter(pathmodifier.Diffeo):
     def __init__(self):
@@ -122,30 +126,30 @@ class PathScatter(pathmodifier.Diffeo):
 
         idList = self.document.get_z_selected()
 
-        ##first selected->pattern, all but first selected-> skeletons
-        #id = self.options.ids[-1]
+        # first selected->pattern, all but first selected-> skeletons
+        # id = self.options.ids[-1]
         id = idList[-1]
-        self.patternNode=self.selected[id]
+        self.patternNode = self.selected[id]
 
         self.gNode = etree.Element('{http://www.w3.org/2000/svg}g')
         self.patternNode.getparent().append(self.gNode)
 
-        if self.options.copymode=="copy":
-            duplist=self.duplicateNodes({id:self.patternNode})
+        if self.options.copymode == "copy":
+            duplist = self.duplicateNodes({id: self.patternNode})
             self.patternNode = list(duplist.values())[0]
 
-        #TODO: allow 4th option: duplicate the first copy and clone the next ones.
-        if "%s"%self.options.copymode=="clone":
+        # TODO: allow 4th option: duplicate the first copy and clone the next ones.
+        if "%s" % self.options.copymode == "clone":
             self.patternNode = etree.Element('{http://www.w3.org/2000/svg}use')
-            self.patternNode.set('{http://www.w3.org/1999/xlink}href',"#%s"%id)
+            self.patternNode.set('{http://www.w3.org/1999/xlink}href', "#%s" % id)
             self.gNode.append(self.patternNode)
 
-        self.skeletons=self.selected
+        self.skeletons = self.selected
         del self.skeletons[id]
         self.expandGroupsUnlinkClones(self.skeletons, True, False)
-        self.objectsToPaths(self.skeletons,False)
+        self.objectsToPaths(self.skeletons, False)
 
-    def lengthtotime(self,l):
+    def lengthtotime(self, l):
         """
         Receives an arc length l, and returns the index of the segment in self.skelcomp
         containing the corresponding point, to gether with the position of the point on this segment.
@@ -153,108 +157,105 @@ class PathScatter(pathmodifier.Diffeo):
         If the deformer is closed, do computations modulo the total length.
         """
         if self.skelcompIsClosed:
-            l=l % sum(self.lengths)
-        if l<=0:
-            return 0,l/self.lengths[0]
-        i=0
-        while (i<len(self.lengths)) and (self.lengths[i]<=l):
-            l-=self.lengths[i]
-            i+=1
-        t=l/self.lengths[min(i,len(self.lengths)-1)]
+            l = l % sum(self.lengths)
+        if l <= 0:
+            return 0, l / self.lengths[0]
+        i = 0
+        while (i < len(self.lengths)) and (self.lengths[i] <= l):
+            l -= self.lengths[i]
+            i += 1
+        t = l / self.lengths[min(i, len(self.lengths) - 1)]
         return i, t
 
-    def localTransformAt(self,s,follow=True):
+    def localTransformAt(self, s, follow=True):
         """
         receives a length, and returns the corresponding point and tangent of self.skelcomp
         if follow is set to false, returns only the translation
         """
-        i,t=self.lengthtotime(s)
-        if i==len(self.skelcomp)-1:
-            x,y=inkex.between_point(self.skelcomp[i-1],self.skelcomp[i],1+t)
-            dx=(self.skelcomp[i][0]-self.skelcomp[i-1][0])/self.lengths[-1]
-            dy=(self.skelcomp[i][1]-self.skelcomp[i-1][1])/self.lengths[-1]
+        i, t = self.lengthtotime(s)
+        if i == len(self.skelcomp) - 1:
+            x, y = inkex.between_point(self.skelcomp[i - 1], self.skelcomp[i], 1 + t)
+            dx = (self.skelcomp[i][0] - self.skelcomp[i - 1][0]) / self.lengths[-1]
+            dy = (self.skelcomp[i][1] - self.skelcomp[i - 1][1]) / self.lengths[-1]
         else:
-            x,y=inkex.between_point(self.skelcomp[i],self.skelcomp[i+1],t)
-            dx=(self.skelcomp[i+1][0]-self.skelcomp[i][0])/self.lengths[i]
-            dy=(self.skelcomp[i+1][1]-self.skelcomp[i][1])/self.lengths[i]
+            x, y = inkex.between_point(self.skelcomp[i], self.skelcomp[i + 1], t)
+            dx = (self.skelcomp[i + 1][0] - self.skelcomp[i][0]) / self.lengths[i]
+            dy = (self.skelcomp[i + 1][1] - self.skelcomp[i][1]) / self.lengths[i]
         if follow:
-            mat=[[dx,-dy,x],[dy,dx,y]]
+            mat = [[dx, -dy, x], [dy, dx, y]]
         else:
-            mat=[[1,0,x],[0,1,y]]
+            mat = [[1, 0, x], [0, 1, y]]
         return mat
-
 
     def effect(self):
 
-        if len(self.options.ids)<2:
+        if len(self.options.ids) < 2:
             inkex.errormsg(_("This extension requires two selected paths."))
             return
         self.prepareSelectionList()
 
-        #center at (0,0)
+        # center at (0,0)
         bbox = inkex.computeBBox([self.patternNode])
-        mat=[[1,0,-(bbox[0]+bbox[1])/2],[0,1,-(bbox[2]+bbox[3])/2]]
+        mat = [[1, 0, -(bbox[0] + bbox[1]) / 2], [0, 1, -(bbox[2] + bbox[3]) / 2]]
         if self.options.vertical:
-            bbox=[-bbox[3],-bbox[2],bbox[0],bbox[1]]
-            mat = simpletransform.composeTransform([[0,-1,0],[1,0,0]],mat)
+            bbox = [-bbox[3], -bbox[2], bbox[0], bbox[1]]
+            mat = simpletransform.composeTransform([[0, -1, 0], [1, 0, 0]], mat)
         mat[1][2] += self.options.noffset
-        inkex.applyTransformToNode(mat,self.patternNode)
+        inkex.applyTransformToNode(mat, self.patternNode)
 
-        width=bbox[1]-bbox[0]
-        dx=width+self.options.space
+        width = bbox[1] - bbox[0]
+        dx = width + self.options.space
 
-        #check if group and expand it
+        # check if group and expand it
         patternList = []
-        if self.options.grouppick and (self.patternNode.tag == inkex.addNS('g','svg') or self.patternNode.tag=='g') :
+        if self.options.grouppick and (self.patternNode.tag == inkex.addNS('g', 'svg') or self.patternNode.tag == 'g'):
             mat = simpletransform.parseTransform(self.patternNode.get("transform"))
             for child in self.patternNode:
-                inkex.applyTransformToNode(mat,child)
+                inkex.applyTransformToNode(mat, child)
                 patternList.append(child)
-        else :
+        else:
             patternList.append(self.patternNode)
-        #inkex.debug(patternList)
+        # inkex.debug(patternList)
 
-        counter=0
+        counter = 0
         for skelnode in self.skeletons.values():
             self.curSekeleton = parsecubicPath(skelnode.get('d'))
             for comp in self.curSekeleton:
-                self.skelcomp,self.lengths=linearize(comp)
-                #!!!!>----> TODO: really test if path is closed! end point==start point is not enough!
-                self.skelcompIsClosed = (self.skelcomp[0]==self.skelcomp[-1])
+                self.skelcomp, self.lengths = linearize(comp)
+                # !!!!>----> TODO: really test if path is closed! end point==start point is not enough!
+                self.skelcompIsClosed = (self.skelcomp[0] == self.skelcomp[-1])
 
-                length=sum(self.lengths)
+                length = sum(self.lengths)
                 if self.options.stretch:
-                    dx=width+self.options.space
-                    n=int((length-self.options.toffset+self.options.space)/dx)
-                    if n>0:
-                        dx=(length-self.options.toffset)/n
+                    dx = width + self.options.space
+                    n = int((length - self.options.toffset + self.options.space) / dx)
+                    if n > 0:
+                        dx = (length - self.options.toffset) / n
 
+                xoffset = self.skelcomp[0][0] - bbox[0] + self.options.toffset
+                yoffset = self.skelcomp[0][1] - (bbox[2] + bbox[3]) / 2 - self.options.noffset
 
-                xoffset=self.skelcomp[0][0]-bbox[0]+self.options.toffset
-                yoffset=self.skelcomp[0][1]-(bbox[2]+bbox[3])/2-self.options.noffset
+                s = self.options.toffset
+                while s <= length:
+                    mat = self.localTransformAt(s, self.options.follow)
+                    if self.options.pickmode == "rand":
+                        clone = copy.deepcopy(patternList[random.randint(0, len(patternList) - 1)])
 
-                s=self.options.toffset
-                while s<=length:
-                    mat=self.localTransformAt(s,self.options.follow)
-                    if self.options.pickmode=="rand":
-                        clone=copy.deepcopy(patternList[random.randint(0, len(patternList)-1)])
+                    if self.options.pickmode == "seq":
+                        clone = copy.deepcopy(patternList[counter])
+                        counter = (counter + 1) % len(patternList)
 
-                    if self.options.pickmode=="seq":
-                        clone=copy.deepcopy(patternList[counter])
-                        counter=(counter+1)%len(patternList)
-
-                    #!!!--> should it be given an id?
-                    #seems to work without this!?!
-                    myid = patternList[random.randint(0, len(patternList)-1)].tag.split('}')[-1]
+                    # !!!--> should it be given an id?
+                    # seems to work without this!?!
+                    myid = patternList[random.randint(0, len(patternList) - 1)].tag.split('}')[-1]
                     clone.set("id", self.svg.get_unique_id(myid))
                     self.gNode.append(clone)
 
-                    inkex.applyTransformToNode(mat,clone)
+                    inkex.applyTransformToNode(mat, clone)
 
-                    s+=dx
+                    s += dx
         self.patternNode.getparent().remove(self.patternNode)
+
 
 if __name__ == '__main__':
     PathScatter().run()
-
-

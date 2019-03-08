0ec6791fc1ca6bc3d4a2b223f22abe6ec395b38c
Thomas Holder <thomas.holder@schrodinger.com>
2019-03-08 12:00:46 +0100

    remove all hard-coded sys.path modification

--- jessyInk_export.py
+++ jessyInk_export.py
@@ -13,16 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see http://www.gnu.org/licenses/.
 
-# These lines are only needed if you don't put the script directly into
-# the installation directory
 import sys
-# Unix
-sys.path.append('/usr/share/inkscape/extensions')
-# OS X
-sys.path.append('/Applications/Inkscape.app/Contents/Resources/extensions')
-# Windows
-sys.path.append('C:\Program Files\Inkscape\share\extensions')
-
 import inkex, os.path
 import subprocess
 import tempfile

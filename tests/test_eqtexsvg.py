40f053da73ae95d16f1f98bc441f063ef5ef7b86
Martin Owens <doctormo@gmail.com>
2019-03-30 12:07:12 -0400

    Remove eqtexsvg extension, requires pstoedit dependancy, making it ineligable for core extensions.

--- tests/test_eqtexsvg.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from eqtexsvg import EQTEXSVG
-from tests.base import InkscapeExtensionTestMixin, TestCase
-
-
-class EQTEXSVGBasicTest(InkscapeExtensionTestMixin, TestCase):
-    def setUp(self):
-        self.effect = EQTEXSVG
-        self.e = self.effect()

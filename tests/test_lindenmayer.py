d6fb2496f09d3076255bdb05b75d60ca5faeaebf
Martin Owens <doctormo@gmail.com>
2019-03-30 23:47:33 -0400

    Forgot style filter on lindenmayer test

--- tests/test_lindenmayer.py
+++ tests/test_lindenmayer.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 from lindenmayer import LSystem
 from tests.base import ComparisonMixin, InkscapeExtensionTestMixin, TestCase
-
+from tests.base.filters import CompareOrderIndependentStyle
 
 class LSystemBasicTest(ComparisonMixin, InkscapeExtensionTestMixin, TestCase):
     effect = LSystem
+    compare_filters = [CompareOrderIndependentStyle()]

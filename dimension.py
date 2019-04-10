641c31280a26d72617469877b9a326192226e7f2
Martin Owens <doctormo@gmail.com>
2019-04-09 21:44:53 -0400

    Further fix the dimension, remove use of Popen to get boundingbox

--- dimension.py
+++ dimension.py
@@ -18,7 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 """
-dimension.py
 An Inkscape effect for adding CAD style dimensions to selected objects
 in a drawing.
 
@@ -34,44 +33,43 @@ This code contains snippets from existing effects in the Inkscape
 extensions library, and marker data from markers.svg.
 """
 
-from subprocess import PIPE, Popen
-
-from lxml import etree
-
 import inkex
-import pathmodifier
+from inkex.transforms import BoundingBox
+from inkex.elements import Group, Marker, PathElement
 from inkex.localize import _
 
+import pathmodifier
 
 class Dimension(pathmodifier.PathModifier):
     """Add dimensions as a path modifier"""
 
     def __init__(self):
         super(Dimension, self).__init__()
+        self.bbox = BoundingBox(None)
+        self.xoffset = 0
+        self.yoffset = 0
         self.arg_parser.add_argument(
-                "-x", "--xoffset", type=float, dest="xoffset", default=100.0,
-                help="x offset of the vertical dimension arrow")
+            "-x", "--xoffset", type=float, dest="xoffset", default=100.0,
+            help="x offset of the vertical dimension arrow")
         self.arg_parser.add_argument(
-                "-y", "--yoffset", type=float, dest="yoffset", default=100.0,
-                help="y offset of the horizontal dimension arrow")
+            "-y", "--yoffset", type=float, dest="yoffset", default=100.0,
+            help="y offset of the horizontal dimension arrow")
         self.arg_parser.add_argument(
-                "-t", "--type", type=str, dest="type", default="geometric",
-                help="Bounding box type")
+            "-t", "--type", type=str, dest="type", default="geometric",
+            help="Bounding box type")
 
-    def addMarker(self, name, rotate):
-        defs = self.svg.getElement('/svg:svg//svg:defs')
-        if defs is None:
-            defs = etree.SubElement(self.document.getroot(), inkex.addNS('defs', 'svg'))
-        marker = etree.SubElement(defs, inkex.addNS('marker', 'svg'))
+    def add_marker(self, name, rotate):
+        """Create a marker in the defs of the svg"""
+        marker = Marker()
         marker.set('id', name)
         marker.set('orient', 'auto')
         marker.set('refX', '0.0')
         marker.set('refY', '0.0')
         marker.set('style', 'overflow:visible')
-        marker.set(inkex.addNS('stockid', 'inkscape'), name)
+        marker.set('inkscape:stockid', name)
+        self.svg.defs.append(marker)
 
-        arrow = etree.Element("path")
-        arrow.set('d', 'M 0.0,0.0 L 5.0,-5.0 L -12.5,0.0 L 5.0,5.0 L 0.0,0.0 z ')
+        arrow = PathElement(d='M 0.0,0.0 L 5.0,-5.0 L -12.5,0.0 L 5.0,5.0 L 0.0,0.0 z ')
         if rotate:
             arrow.set('transform', 'scale(0.8) rotate(180) translate(12.5,0)')
         else:
@@ -79,19 +77,21 @@ class Dimension(pathmodifier.PathModifier):
         arrow.set('style', 'fill-rule:evenodd;stroke:#000000;stroke-width:1.0pt;marker-start:none')
         marker.append(arrow)
 
-    def dimHLine(self, y, xlat):
-        line = etree.Element("path")
-        x1 = self.bbox[0] - xlat[0] * self.xoffset
-        x2 = self.bbox[1]
+    def horz_line(self, y, xlat):
+        """Create a horzontal line"""
+        line = PathElement()
+        x1 = self.bbox.left - xlat[0] * self.xoffset
+        x2 = self.bbox.right
         y = y - xlat[1] * self.yoffset
         line.set('d', 'M %f %f H %f' % (x1, y, x2))
         return line
 
-    def dimVLine(self, x, xlat):
-        line = etree.Element("path")
+    def vert_line(self, x, xlat):
+        """Create a vertical line"""
+        line = PathElement()
         x = x - xlat[0] * self.xoffset
-        y1 = self.bbox[2] - xlat[1] * self.yoffset
-        y2 = self.bbox[3]
+        y1 = self.bbox.top - xlat[1] * self.yoffset
+        y2 = self.bbox.bottom
         line.set('d', 'M %f %f V %f' % (x, y1, y2))
         return line
 
@@ -100,71 +100,57 @@ class Dimension(pathmodifier.PathModifier):
         self.xoffset = scale * self.options.xoffset
         self.yoffset = scale * self.options.yoffset
 
-        # query inkscape about the bounding box
-        if len(self.options.ids) == 0:
+        if not self.svg.selected:
             return inkex.errormsg(_("Please select an object."))
         if self.options.type == "geometric":
             self.bbox = self.svg.get_selected_bbox()
         else:
-            q = {'x': 0, 'y': 0, 'width': 0, 'height': 0}
-            file = self.options.input_file
-            id = self.options.ids[0]
-            for query in q.keys():
-                p = Popen('inkscape --query-%s --query-id=%s "%s"' % (query, id, file), shell=True, stdout=PIPE, stderr=PIPE)
-                rc = p.wait()
-                q[query] = scale * float(p.stdout.read())
-                err = p.stderr.read()
-
-            self.bbox = (q['x'], q['x'] + q['width'], q['y'], q['y'] + q['height'])
-
-        # Avoid ugly failure on rects and texts.
-        try:
-            testing_the_water = self.bbox[0]
-        except TypeError:
-            return inkex.errormsg(_('Unable to process this object.  Try changing it into a path first.'))
+            self.bbox = self.svg.selected.values()[0].bounding_box()
+            self.bbox *= scale
 
         layer = self.svg.get_current_layer()
 
-        self.addMarker('Arrow1Lstart', False)
-        self.addMarker('Arrow1Lend', True)
+        self.add_marker('Arrow1Lstart', False)
+        self.add_marker('Arrow1Lend', True)
 
-        group = etree.SubElement(layer, 'g')
-        # group = etree.Element("g")
+        group = Group()
+        layer.append(group)
         group.set('fill', 'none')
         group.set('stroke', 'black')
 
-        line = self.dimHLine(self.bbox[2], [0, 1])
+        line = self.horz_line(self.bbox.top, [0, 1])
         line.set('marker-start', 'url(#Arrow1Lstart)')
         line.set('marker-end', 'url(#Arrow1Lend)')
         line.set('stroke-width', str(scale))
         group.append(line)
 
-        line = self.dimVLine(self.bbox[0], [0, 2])
+        line = self.vert_line(self.bbox.left, [0, 2])
         line.set('stroke-width', str(0.5 * scale))
         group.append(line)
 
-        line = self.dimVLine(self.bbox[1], [0, 2])
+        line = self.vert_line(self.bbox.right, [0, 2])
         line.set('stroke-width', str(0.5 * scale))
         group.append(line)
 
-        line = self.dimVLine(self.bbox[0], [1, 0])
+        line = self.vert_line(self.bbox.left, [1, 0])
         line.set('marker-start', 'url(#Arrow1Lstart)')
         line.set('marker-end', 'url(#Arrow1Lend)')
         line.set('stroke-width', str(scale))
         group.append(line)
 
-        line = self.dimHLine(self.bbox[2], [2, 0])
+        line = self.horz_line(self.bbox.top, [2, 0])
         line.set('stroke-width', str(0.5 * scale))
         group.append(line)
 
-        line = self.dimHLine(self.bbox[3], [2, 0])
+        line = self.horz_line(self.bbox.bottom, [2, 0])
         line.set('stroke-width', str(0.5 * scale))
         group.append(line)
 
-        for id, node in self.svg.selected.items():
+        for node in self.svg.selected.values():
             group.append(node)
 
         layer.append(group)
+        return None
 
 
 if __name__ == '__main__':

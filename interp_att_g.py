f26155de5af6780e614d491241449e5325efad75
Martin Owens <doctormo@gmail.com>
2019-04-10 11:41:50 -0400

    Improve and add comparison test for interp_att_g extension

--- interp_att_g.py
+++ interp_att_g.py
@@ -17,214 +17,165 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
+"""
+Interpolation of attributes in selected objects or group's children.
+"""
 
-import re
 import inkex
 from inkex import inkbool
 from inkex.generic import EffectExtension
+from inkex.colors import Color
+from inkex.elements import Group
+from inkex.localize import _
 
 class InterpAttG(EffectExtension):
+    """
+    This effect applies a value for any interpolatable attribute for all
+    elements inside the selected group or for all elements in a multiple selection.
+    """
     def __init__(self):
         super(InterpAttG, self).__init__()
-        self.arg_parser.add_argument("-a", "--att",
-                         type=str,
-                        dest="att", default="fill",
-                        help="Attribute to be interpolated.")
-        self.arg_parser.add_argument("-o", "--att-other",
-                         type=str,
-                        dest="att_other",
-                        help="Other attribute (for a limited UI).")
-        self.arg_parser.add_argument("-t", "--att-other-type",
-                         type=str,
-                        dest="att_other_type",
-                        help="The other attribute type.")
-        self.arg_parser.add_argument("-w", "--att-other-where",
-                         type=str,
-                        dest="att_other_where",
-                        help="That is a tag attribute or a style attribute?")
-        self.arg_parser.add_argument("-s", "--start-val",
-                         type=str,
-                        dest="start_val", default="#F00",
-                        help="Initial interpolation value.")
-        self.arg_parser.add_argument("-e", "--end-val",
-                         type=str,
-                        dest="end_val", default="#00F",
-                        help="End interpolation value.")
-        self.arg_parser.add_argument("-u", "--unit",
-                         type=str,
-                        dest="unit", default="color",
-                        help="Values unit.")
-        self.arg_parser.add_argument("--zsort",
-                         type=inkbool,
-                        dest="zsort", default=True,
-                        help="use z-order instead of selection order")
-        self.arg_parser.add_argument("--tab",
-                         type=str,
-                        dest="tab",
-                        help="The selected UI-tab when OK was pressed")
+        self.arg_parser.add_argument(
+            "-a", "--att", type=str, dest="att", default="fill",
+            help="Attribute to be interpolated.")
+        self.arg_parser.add_argument(
+            "-o", "--att-other", type=str, dest="att_other",
+            help="Other attribute (for a limited UI).")
+        self.arg_parser.add_argument(
+            "-t", "--att-other-type", type=str, dest="att_other_type",
+            help="The other attribute type.")
+        self.arg_parser.add_argument(
+            "-w", "--att-other-where", type=str, dest="att_other_where",
+            help="That is a tag attribute or a style attribute?")
+        self.arg_parser.add_argument(
+            "-s", "--start-val", type=str, dest="start_val", default="#F00",
+            help="Initial interpolation value.")
+        self.arg_parser.add_argument(
+            "-e", "--end-val", type=str, dest="end_val", default="#00F",
+            help="End interpolation value.")
+        self.arg_parser.add_argument(
+            "-u", "--unit", type=str, dest="unit", default="color",
+            help="Values unit.")
+        self.arg_parser.add_argument(
+            "--zsort", type=inkbool, dest="zsort", default=True,
+            help="use z-order instead of selection order")
+        self.arg_parser.add_argument(
+            "--tab", type=str, dest="tab",
+            help="The selected UI-tab when OK was pressed")
 
-    def getColorValues(self):
-        sv = self.options.start_val.lstrip('#')
-        ev = self.options.end_val.lstrip('#')
+    def get_color_steps(self, total):
+        """Get the color value, returning the start color and a single increment step"""
+        start_value = Color(self.options.start_val)
+        end_value = Color(self.options.end_val)
 
-        # index 0: start color, index 1: end color
-        self.R, self.G, self.B = [0,0],[0,0],[0,0]
-        raw_colors = [sv, ev]
+        color_inc = [
+            (end_value[v] - start_value[v]) / float(total - 1)
+            for v in range(3)]
 
-        for i in [0,1]:
-            if re.search('\\s|,', raw_colors[i]):
-                # There are separators. That must be an integer RGB color definition.
-                raw_colors[i] = re.split( '[\\s,]+', raw_colors[i])
-                self.R[i] = int(raw_colors[i][0])
-                self.G[i] = int(raw_colors[i][1])
-                self.B[i] = int(raw_colors[i][2])
-            else:
-                # There is no separator. That must be a Hex RGB color definition.
-                if len(raw_colors[i]) == 3:
-                    self.R[i] = int(raw_colors[i][0] + raw_colors[i][0], 16)
-                    self.G[i] = int(raw_colors[i][1] + raw_colors[i][1], 16)
-                    self.B[i] = int(raw_colors[i][2] + raw_colors[i][2], 16)
-                else: # the len must be 6
-                    self.R[i] = int( raw_colors[i][0] + raw_colors[i][1], 16)
-                    self.G[i] = int( raw_colors[i][2] + raw_colors[i][3], 16)
-                    self.B[i] = int( raw_colors[i][4] + raw_colors[i][5], 16)
+        return start_value, color_inc
 
-        self.R_inc = (self.R[1] - self.R[0]) / float(self.tot_el - 1)
-        self.G_inc = (self.G[1] - self.G[0]) / float(self.tot_el - 1)
-        self.B_inc = (self.B[1] - self.B[0]) / float(self.tot_el - 1)
-        self.R_cur = self.R[0]
-        self.G_cur = self.G[0]
-        self.B_cur = self.B[0]
-
-    def getNumberValues(self):
-        sv = self.options.start_val.replace(",", ".")
-        ev = self.options.end_val.replace(",", ".")
+    def get_number_steps(self, total):
+        """Get the number value, returning the start float and a single increment step"""
+        start_value = self.options.start_val.replace(",", ".")
+        end_value = self.options.end_val.replace(",", ".")
         unit = self.options.unit
 
         if unit != 'none':
-            sv = self.svg.unittouu(sv + unit)
-            ev = self.svg.unittouu(ev + unit)
-        else:
-            sv = float(sv)
-            ev = float(ev)
-        self.val_cur = self.val_ini = sv
-        self.val_end = ev
-        self.val_inc = (ev - sv)/float(self.tot_el - 1)
+            start_value = self.svg.unittouu(start_value + unit)
+            end_value = self.svg.unittouu(end_value + unit)
 
-    def getTotElements(self):
-        self.tot_el = 0
-        self.collection = None
-        if len( self.svg.selected ) == 0:
-            return False
-        if len( self.svg.selected ) > 1:
+        try:
+            start_value = float(start_value)
+            end_value = float(end_value)
+        except ValueError:
+            inkex.errormsg(
+                _("Bad values for a number field: {}, {}.".format(start_value, end_value)))
+            return 0, 0
+
+        val_inc = (end_value - start_value) / float(total - 1)
+        return start_value, val_inc
+
+    def get_elements(self):
+        """Returns a list of elements to work on"""
+        if not self.svg.selected:
+            return []
+
+        if len(self.svg.selected) > 1:
             # multiple selection
             if self.options.zsort:
-                sorted_ids = self.document.get_z_selected()
-            else:
-                sorted_ids = self.document.selected
+                return self.svg.get_z_selected().values()
+            return self.svg.selected.values()
 
-            self.collection = list(sorted_ids)
-            for i in sorted_ids:
-                path = '//*[@id="%s"]' % i
-                self.collection[self.tot_el] = self.document.xpath(path, namespaces=inkex.NSS)[0]
-                self.tot_el += 1
-        else:
-            # must be a group
-            self.collection = self.selected[ self.options.ids[0] ]
-            for i in self.collection:
-                self.tot_el += 1
+        # must be a group
+        node = self.svg.get_first_selected()
+        if isinstance(node, Group):
+            return list(node)
+        return []
 
     def effect(self):
         if self.options.att == 'other':
             if self.options.att_other is not None:
-                self.inte_att = self.options.att_other
+                inte_att = self.options.att_other
             else:
                 inkex.errormsg(_("You selected 'Other'. Please enter an attribute to interpolate."))
                 return
 
-            self.inte_att_type = self.options.att_other_type
-            self.where = self.options.att_other_where
+            inte_att_type = self.options.att_other_type
+            where = self.options.att_other_where
         else:
-            self.inte_att = self.options.att
-            if self.inte_att == 'width':
-                self.inte_att_type=float
-                self.where = 'tag'
-            elif self.inte_att == 'height':
-                self.inte_att_type=float
-                self.where = 'tag'
-            elif self.inte_att == 'scale':
-                self.inte_att_type=float
-                self.where = 'transform'
-            elif self.inte_att == 'trans-x':
-                self.inte_att_type=float
-                self.where = 'transform'
-            elif self.inte_att == 'trans-y':
-                self.inte_att_type=float
-                self.where = 'transform'
-            elif self.inte_att == 'fill':
-                self.inte_att_type = 'color'
-                self.where = 'style'
-            elif self.inte_att == 'opacity':
-                self.inte_att_type=float
-                self.where = 'style'
+            inte_att = self.options.att
+            inte_att_type = 'float'
+            if inte_att in ('width', 'height'):
+                where = 'tag'
+            elif inte_att in ('scale', 'trans-x', 'trans-y'):
+                where = 'transform'
+            elif inte_att == 'opacity':
+                where = 'style'
+            elif inte_att in ('fill', 'stroke'):
+                inte_att_type = 'color'
+                where = 'style'
 
-        self.getTotElements()
+        collection = self.get_elements()
 
-        if self.inte_att_type == 'color':
-            self.getColorValues()
-        else:
-            self.getNumberValues()
-
-        if self.collection is None:
-            inkex.errormsg( _('There is no selection to interpolate' ))
+        if not collection:
+            inkex.errormsg(_('There is no selection to interpolate'))
             return False
 
-        for node in self.collection:
-            if self.inte_att_type == 'color':
-                val = 'rgb('+ \
-                    str(int(round(self.R_cur))) +','+ \
-                    str(int(round(self.G_cur))) +','+ \
-                    str(int(round(self.B_cur))) +')'
-            else:
-                if self.inte_att_type == 'float':
-                    val = self.val_cur
-                else: # inte_att_type == 'int'
-                    val = int(round(self.val_cur))
+        if inte_att_type == 'color':
+            cur, inc = self.get_color_steps(len(collection))
+        else:
+            cur, inc = self.get_number_steps(len(collection))
 
-            if self.where == 'style':
-                s = node.get('style')
-                re_find = '(^|;)'+ self.inte_att +':[^;]*(;|$)'
-                if re.search( re_find, s ):
-                    s = re.sub( re_find, '\\1'+ self.inte_att +':'+ str(val) +'\\2', s )
-                else:
-                    s += ';'+ self.inte_att +':'+ str(val)
-                node.set( 'style', s )
-            elif self.where == 'transform':
-                t = node.get('transform')
-                if t == None: t = ""
-                if self.inte_att == 'trans-x':
-                    val = "translate("+ str(val) +",0)"
-                elif self.inte_att == 'trans-y':
-                    val = "translate(0,"+ str(val) +")"
-                else:
-                    val = self.inte_att + "("+ str(val) +")"
-                node.set( 'transform', t +" "+ val )
-            else: # self.where == 'tag':
-                if ":" in self.inte_att:
-                    ns, attrib = self.inte_att.split(":")
-                    node.set(inkex.addNS(attrib, ns), str(val))
-                else:
-                    node.set( self.inte_att, str(val) )
-
-            if self.inte_att_type == 'color':
-                self.R_cur += self.R_inc
-                self.G_cur += self.G_inc
-                self.B_cur += self.B_inc
+        for node in collection:
+            if inte_att_type == 'color':
+                val = str(cur)
+            elif inte_att_type == 'float':
+                val = cur
+            elif inte_att_type == 'int':
+                val = int(round(cur))
             else:
-                self.val_cur += self.val_inc
+                raise KeyError("Unknown attr type: {}".format(inte_att_type))
+
+            if where == 'style':
+                node.style[inte_att] = str(val)
+            elif where == 'transform':
+                if inte_att == 'trans-x':
+                    node.transform.add_translate(val, 0)
+                elif inte_att == 'trans-y':
+                    node.transform.add_translate(0, val)
+                elif inte_att == 'scale':
+                    node.transform.add_scale(val)
+            elif where == 'tag':
+                node.set(inte_att, str(val))
+            else:
+                raise KeyError("Unknown update {}".format(where))
+
+            if inte_att_type == 'color':
+                cur = Color([cur[i] + inc[i] for i in range(3)])
+            else:
+                cur += inc
 
         return True
 
 if __name__ == '__main__':
     InterpAttG().run()
-
--- tests/test_interp_att_g.py
+++ tests/test_interp_att_g.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
 from interp_att_g import InterpAttG
-from tests.base import InkscapeExtensionTestMixin, TestCase
+from tests.base import ComparisonMixin, InkscapeExtensionTestMixin, TestCase
 
-class InterpAttGBasicTest(InkscapeExtensionTestMixin, TestCase):
+class InterpAttGBasicTest(ComparisonMixin, InkscapeExtensionTestMixin, TestCase):
     effect_class = InterpAttG
+    comparisons = [('--id=layer1',)]

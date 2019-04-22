71f7ba8ef49c408debd93be311d92bfeaba31f5e
Martin Owens <doctormo@gmail.com>
2019-04-22 18:36:47 -0400

    Add replacement dwh input (aka dm2svg) extension with comparison tests.

---
+++ tests/test_dhw_input.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+from dhw_input import DhwInput
+
+from tests.base import ComparisonMixin, InkscapeExtensionTestMixin, TestCase
+from tests.base.filters import CompareNumericFuzzy
+
+
+class TestDxfInput(ComparisonMixin, InkscapeExtensionTestMixin, TestCase):
+    effect_class = DhwInput
+    compare_file = [
+        'dhw/PAGE_001.DHW',
+        'dhw/PGLT_161.DHW',
+        'dhw/PGLT_162.DHW',
+        'dhw/PGLT_163.DHW',
+    ]
+    compare_filters = [CompareNumericFuzzy()]
+    comparisons = [()]

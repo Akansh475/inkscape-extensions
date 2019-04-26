4c427ba65c401238e9a16c60e8080113280aae0f
Martin Owens <doctormo@gmail.com>
2019-04-26 17:11:24 -0400

    Remove test run_command

--- tests/test_run_command.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from run_command import run
-
-
-class TestRunCommand(object):
-    def test_basic_run(self):
-        # Make sure a benign command doesn't raise any exceptions
-        assert run("touch %s", 'touch') is None

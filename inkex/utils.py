Akansh475/inkscape-extensions

commit 7fea47c59300aaa80eef840fabc2026529476c51

Merge branch 'master' into 'fix_unicode_in_tests'
# Conflicts:
#   tests/test_coverage.py
#   tests/test_frame.py

inkex/utils.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 #
 # Copyright (C) 2010 Nick Drobchenko, nick@cnc-club.ru
 # Copyright (C) 2005 Aaron Spike, aaron@ekips.org
@@ -19,6 +20,8 @@
 """
 Basic common utility functions for calculated things
 """
+from __future__ import absolute_import, print_function, unicode_literals
+
 
 import os
 import sys
@@ -36,15 +39,15 @@ if PY3:
 
 # a dictionary of all of the xmlns prefixes in a standard inkscape doc
 NSS = {
-    u'sodipodi' :u'http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd',
-    u'cc'       :u'http://creativecommons.org/ns#',
-    u'ccOLD'    :u'http://web.resource.org/cc/',
-    u'svg'      :u'http://www.w3.org/2000/svg',
-    u'dc'       :u'http://purl.org/dc/elements/1.1/',
-    u'rdf'      :u'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-    u'inkscape' :u'http://www.inkscape.org/namespaces/inkscape',
-    u'xlink'    :u'http://www.w3.org/1999/xlink',
-    u'xml'      :u'http://www.w3.org/XML/1998/namespace'
+    'sodipodi' :'http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd',
+    'cc'       :'http://creativecommons.org/ns#',
+    'ccOLD'    :'http://web.resource.org/cc/',
+    'svg'      :'http://www.w3.org/2000/svg',
+    'dc'       :'http://purl.org/dc/elements/1.1/',
+    'rdf'      :'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    'inkscape' :'http://www.inkscape.org/namespaces/inkscape',
+    'xlink'    :'http://www.w3.org/1999/xlink',
+    'xml'      :'http://www.w3.org/XML/1998/namespace'
 }
 SSN = dict((b, a) for (a, b) in NSS.items())
 
@@ -57,7 +60,7 @@ def inkbool(value):
 
 def debug(what):
     """Print debug message if debugging is switched on"""
-    sys.stderr.write(str(what) + "\n")
+    sys.stderr.write(unicode(what) + "\n")
     return what
 
 def errormsg(msg):
@@ -73,12 +76,7 @@ def errormsg(msg):
          ...
          inkex.errormsg(_("This extension requires two selected paths."))
     """
-    if PY3:
-        sys.stderr.write(msg + "\n")
-    elif isinstance(msg, unicode):
-        sys.stderr.write(msg.encode("utf-8") + "\n")
-    else:
-        sys.stderr.write((unicode(msg, "utf-8", errors='replace') + "\n").encode("utf-8"))
+    sys.stderr.write(unicode(msg) + "\n")
 
 class AbortExtension(Exception):
     """Raised to print a message to the user without backtrace"""

tests/test_inkex_utils.py
@@ -6,6 +6,7 @@ Unit test file for ../inkex.py
 # Revision history:
 #   * 2012-01-27 (jazzynico): check errormsg function.
 #
+from __future__ import absolute_import, print_function, unicode_literals
 
 import unittest
 from argparse import ArgumentTypeError
@@ -32,7 +33,7 @@ class InkexBasicTest(TestCase):
         """Debug messages go to stderr"""
         with StdRedirect('stderr') as err:
             debug("Hello World")
-            self.assertEqual(str(err), 'Hello World\n')
+            self.assertEqual(err.str, 'Hello World\n')
 
     def test_to(self):
         """Decorator for generators"""
@@ -90,19 +91,21 @@ class InkexBasicTest(TestCase):
         """Parse ABCabc"""
         with StdRedirect('stderr') as err:
             errormsg('ABCabc')
-            self.assertEqual(str(err), 'ABCabc\n')
+            self.assertEqual(err.str, 'ABCabc\n')
 
     def test_nonunicode_latin1(self):
-        """Parse Àûïàèé"""
+        # Py2 has issues with unicode in docstrings.   *sigh*
+        # """Parse Àûïàèé"""
         with StdRedirect('stderr') as err:
             errormsg('Àûïàèé')
-            self.assertEqual(str(err), 'Àûïàèé\n')
+            self.assertEqual(err.str, 'Àûïàèé\n')
 
     def test_unicode_latin1(self):
-        """Parse Àûïàèé (unicode)"""
+        # Py2 has issues with unicode in docstrings.   *sigh*
+        # """Parse Àûïàèé (unicode)"""
         with StdRedirect('stderr') as err:
-            errormsg(u'Àûïàèé')
-            self.assertEqual(str(err), 'Àûïàèé\n')
+            errormsg('Àûïàèé')
+            self.assertEqual(err.str, 'Àûïàèé\n')
 
 
 if __name__ == '__main__':

dxf12_outlines.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 #
 # Copyright (C) 2005,2007 Aaron Spike, aaron@ekips.org
 # - template dxf_outlines.dxf added Feb 2008 by Alvin Penner, penner@vaxxine.com
 # - layers, transformation, flattening added April 2008 by Bob Cook, bob@bobcookdev.com
-# - added support for dxf R12, Nov. 2008 by Switcher 
+# - added support for dxf R12, Nov. 2008 by Switcher
 # - brought together to replace ps2edit version 2018 by Martin Owens
 #
 # This program is free software; you can redistribute it and/or modify
@@ -21,8 +21,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-from __future__ import print_function
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function, unicode_literals
+
 
 import re
 import inkex
@@ -63,30 +63,30 @@ EOF'''
 class MyEffect(inkex.Effect):
 
     def __init__(self):
-    
+
         inkex.Effect.__init__(self)
         self.dxf = ''
         self.handle = 255
         self.flatness = 0.1
-        
+
     def output(self):
         print(self.dxf)
-        
+
     def dxf_add(self, str):
         self.dxf += str
-        
+
     def dxf_insert_code(self, code, value):
         self.dxf += code + "\n" + value + "\n"
-        
+
     def dxf_line(self,layer,csp):
         self.dxf_insert_code(   '0', 'LINE' )
         self.dxf_insert_code(   '8', layer )
         ######self.dxf_insert_code(  '62', '1' )  #Change the Line Color
         self.dxf_insert_code(  '10', '%f' % csp[0][0] )
-        self.dxf_insert_code(  '20', '%f' % csp[0][1] ) 
+        self.dxf_insert_code(  '20', '%f' % csp[0][1] )
         self.dxf_insert_code(  '11', '%f' % csp[1][0] )
         self.dxf_insert_code(  '21', '%f' % csp[1][1] )
-        
+
     def dxf_path_to_lines(self,layer,p):
         f = self.flatness
         is_flat = 0
@@ -96,14 +96,14 @@ class MyEffect(inkex.Effect):
                 is_flat = 1
             except:
                 f += 0.1
-        
+
         for sub in p:
             for i in range(len(sub)-1):
                 self.handle += 1
                 s = sub[i]
                 e = sub[i+1]
                 self.dxf_line(layer,[s[1],e[1]])
-    
+
     def dxf_path_to_point(self,layer,p):
         bbox = inkex.roughBBox(p)
         x = (bbox[0] + bbox[1]) / 2
@@ -113,36 +113,36 @@ class MyEffect(inkex.Effect):
     def effect(self):
         self.dxf_insert_code( '999', '"DXF R12 Output" (www.mydxf.blogspot.com)' )
         self.dxf_add( r12_header )
-        
+
         scale = 25.4/90.0
         h = self.svg.unittouu(self.svg.height)
-        
+
         path = '//svg:path'
         for node in self.document.getroot().xpath(path, namespaces=inkex.NSS):
-        
+
             layer = node.getparent().get(inkex.addNS('label','inkscape'))
             if layer == None:
                layer = 'Layer 1'
-            
+
             d = node.get('d')
             p = inkex.parseCubicPath(d)
-            
+
             t = node.get('transform')
             if t != None:
                 m = simpletransform.parseTransform(t)
                 inkex.applyTransformToPath(m,p)
-            
+
             m = [[scale,0,0],[0,-scale,h*scale]]
             inkex.applyTransformToPath(m,p)
-    
+
             if re.search('drill$',layer,re.I) == None:
             #if layer == 'Brackets Drill':
                 self.dxf_path_to_lines(layer,p)
-            else:    
+            else:
                 self.dxf_path_to_point(layer,p)
-                
+
         self.dxf_add( r12_footer )
-        
+
 if __name__ == '__main__':
     e = MyEffect()
     e.affect()

empty_generic.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+# coding=utf-8
 
 # Written by Tavmjong Bah
+from __future__ import absolute_import, print_function, unicode_literals
+
 
 import inkex
 from inkex.utils import inkbool

export_gimp_palette.py
@@ -20,7 +20,7 @@
 Export a gimp pallet file (.gpl)
 """
 
-from __future__ import print_function
+from __future__ import absolute_import, print_function, unicode_literals
 
 import inkex
 

inkex/__init__.py
@@ -1,4 +1,4 @@
-
+# coding=utf-8
 from __future__ import print_function
 
 from .effect import Effect

inkex/base.py
@@ -18,6 +18,8 @@
 """
 The utimate base functionality for every inkscape extension.
 """
+from __future__ import absolute_import, print_function, unicode_literals
+
 
 import sys
 import copy
@@ -25,7 +27,8 @@ import copy
 from argparse import ArgumentParser
 
 from .utils import filename_arg, AbortExtension
-from .svg import etree, SVG_PARSER
+from .svg import SVG_PARSER
+from lxml import etree
 
 class InkscapeExtension(object):
     """
@@ -59,19 +62,15 @@ class InkscapeExtension(object):
 
     def run(self, args=None):
         """Main entrypoint for any Inkscape Extension"""
-        def binary(stream):
-            """For Python 3: Get the underlying binary handle if available"""
-            return getattr(stream, 'buffer', stream)
-
         if args is None:
             args = sys.argv[1:]
 
         self.options = self.arg_parser.parse_args(args)
         if self.options.input_file is None:
-            self.options.input_file = binary(sys.stdin)
+            self.options.input_file = sys.stdin
 
         if self.options.output is None:
-            self.options.output = binary(sys.stdout)
+            self.options.output = sys.stdout
 
         try:
             self.load_raw()
@@ -94,7 +93,7 @@ class InkscapeExtension(object):
         """Save to the output steam, use everything from self"""
         if self.has_changed(ret):
             if isinstance(self.options.output, str):
-                with open(self.options.output, 'wb') as stream:
+                with open(self.options.output, 'w') as stream:
                     self.save(stream)
             else:
                 self.save(self.options.output)
@@ -156,7 +155,7 @@ class SvgOutputMixin(object): # pylint: disable=too-few-public-methods
     """
     def save(self, stream):
         """Save the svg document to the given stream"""
-        self.document.write(stream)
+        stream.write(etree.tostring(self.document).decode())
 
 
 class SvgThroughMixin(SvgInputMixin, SvgOutputMixin):

layout_nup.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
+# coding=utf-8
 #
 # Copyright (C) 2007 Terry Brown, terry_n_brown@yahoo.com
 #
@@ -16,6 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
+from __future__ import absolute_import, unicode_literals
 
 import sys
 import inkex
@@ -83,8 +85,8 @@ class Nup(inkex.Effect):
         node.attributes.setNamedItem(attr)
 
     def output(self):
-        sys.stdout.write(self.pf)
-        
+        sys.stdout.write(self.pf or "")
+
     def expandTuple(self, unit, x, length = 4):
         try:
             iter(x)

commit 8b1d1632971c0e4b4fe4dc3d2e121d1c1107593b

Fix Unicode output for Text -> Extract extension
This prevents errors occurring if text contains non-ASCII characters.
This also fixes Unicode support for AbortExtension.

Note that Inkscape must explicitly request UTF-8 output with
PYTHONIOENCODING, otherwise the text will just be backslash escaped.

Also fix tests to catch this in the future. It appears this was caused
by a regression in commit 7fea47c59300 that made all strings Unicode.

Life would be easier if we just supported Python 3 :(

Fixes: https://gitlab.com/inkscape/extensions/issues/76

inkex/utils.py
@@ -34,10 +34,6 @@ from argparse import ArgumentTypeError
 
 (X, Y) = range(2)
 
-if sys.version_info[0] == 3:  #PY3
-    unicode = str  # pylint: disable=redefined-builtin,invalid-name
-    basestring = str  # pylint: disable=redefined-builtin,invalid-name
-
 # a dictionary of all of the xmlns prefixes in a standard inkscape doc
 NSS = {
     'sodipodi': 'http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd',
@@ -74,9 +70,10 @@ def inkbool(value):
         return False
     return None
 
+
 def debug(what):
     """Print debug message if debugging is switched on"""
-    sys.stderr.write(unicode(what) + "\n")
+    errormsg(what)
     return what
 
 
@@ -93,15 +90,34 @@ def errormsg(msg):
          ...
          inkex.errormsg(_("This extension requires two selected paths."))
     """
-    sys.stderr.write(unicode(msg) + "\n")
+    try:
+        sys.stderr.write(msg)
+    except UnicodeEncodeError:
+        # Python 2:
+        # Fallback for cases where sys.stderr.encoding is not Unicode.
+        # Python 3:
+        # This will not work as write() does not accept byte strings, but AFAIK
+        # we should never reach this point as the default error handler is
+        # 'backslashreplace'.
+
+        # This will be None by default if stderr is piped, so use ASCII as a
+        # last resort.
+        encoding = sys.stderr.encoding or 'ascii'
+        sys.stderr.write(msg.encode(encoding, 'backslashreplace'))
+
+    # Write '\n' separately to avoid dealing with different string types.
+    sys.stderr.write('\n')
 
 
 class AbortExtension(Exception):
     """Raised to print a message to the user without backtrace"""
 
+    def __init__(self, message=""):
+        self.message = message
+
     def write(self):
         """write the error message out to the user"""
-        errormsg(str(self))
+        errormsg(self.message)
 
 
 class DependencyError(NotImplementedError):

text_extract.py
@@ -24,6 +24,7 @@
 #
 
 import copy
+import sys
 
 from lxml import etree
 
@@ -97,7 +98,7 @@ class Extract(inkex.Effect):
             for child in node:
                 if child.get('{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}role'):
                     child.tail = "\n"
-            inkex.errormsg(etree.tostring(node, method='text').strip())
+            inkex.errormsg(etree.tostring(node, encoding='unicode', method='text').strip())
         else:
             for child in node:
                 self.recurse(child)

tests/test_inkex_utils.py
@@ -5,7 +5,7 @@ Unit test file for ../inkex.py
 # Revision history:
 #   * 2012-01-27 (jazzynico): check errormsg function.
 #
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import absolute_import, print_function
 
 from argparse import ArgumentTypeError
 
@@ -85,5 +85,5 @@ class TestInkexBasic(object):
     def test_unicode_latin1(self, capsys):
         # Py2 has issues with unicode in docstrings.   *sigh*
         # """Parse Àûïàèé (unicode)"""
-        errormsg('Àûïàèé')
-        assert capsys.readouterr().err, 'Àûïàèé\n'
+        errormsg(u'Àûïàèé')
+        assert capsys.readouterr().err, u'Àûïàèé\n'
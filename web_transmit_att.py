7a3a558893b89243469e8a6caf6eb68e9f2048f0
Windell Oskay <windell@oskay.net>
2019-07-11 19:15:29 -0700

    Issue #98

--- web_transmit_att.py
+++ web_transmit_att.py
@@ -61,7 +61,7 @@ class InkWebTransmitAtt(inkwebeffect.InkWebEffect):
             idTo.append(self.options.ids[-1])
         else:
             # The first transmit to all
-            elFrom.append(self.selected[self.options.ids[0]])
+            elFrom.append(self.svg.selected[self.options.ids[0]])
             for selId in self.options.ids[1:]:
                 idTo.append(selId)
 

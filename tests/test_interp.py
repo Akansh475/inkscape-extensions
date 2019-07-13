b7479a95a8325902d551e35ac0dd211e31445cf7
Martin Owens <doctormo@gmail.com>
2019-07-13 11:17:58 -0400

    Add test for method=1 in interp extension

--- tests/test_interp.py
+++ tests/test_interp.py
@@ -5,6 +5,7 @@ from inkex.tester import ComparisonMixin, InkscapeExtensionTestMixin, TestCase
 class InterpBasicTest(ComparisonMixin, InkscapeExtensionTestMixin, TestCase):
     effect_class = Interp
     comparisons = [
-        ('--id=a', '--id=b', '--id=c', '--id=d')
+        ('--id=a', '--id=b', '--id=c', '--id=d'),
+        ('--method=1', '--id=a', '--id=b', '--id=c', '--id=d'),
     ]
     compare_file = 'svg/interp_stars.svg'

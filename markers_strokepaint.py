a38ecf2fda9e508cde98591d694a2cb72a7d42d3
Martin Owens <doctormo@gmail.com>
2019-09-16 13:16:21 -0400

    Refactor markers strokepaint extension to fix #132

--- markers_strokepaint.py
+++ markers_strokepaint.py
@@ -25,123 +25,83 @@ import copy
 import inkex
 from inkex.localization import _
 
+MARKERS = ['marker', 'marker-start', 'marker-mid', 'marker-end']
+
 class MarkerStrokePaintEffect(inkex.EffectExtension):
-    def __init__(self):
-        super(MarkerStrokePaintEffect, self).__init__()
-        self.arg_parser.add_argument(
-                "-m", "--modify", type=inkex.Boolean, dest="modify", default=False,
-                help="Do not create a copy, modify the markers")
-        self.arg_parser.add_argument(
-                "-t", "--type", type=str, dest="fill_type", default="stroke",
-                help="Replace the markers' fill with the object stroke or fill color")
-        self.arg_parser.add_argument(
-                "-a", "--alpha", type=inkex.Boolean, dest="assign_alpha", default=True,
-                help="Assign the object fill and stroke alpha to the markers")
-        self.arg_parser.add_argument(
-                "-i", "--invert", type=inkex.Boolean, default=False,
-                help="Invert fill and stroke colors")
-        self.arg_parser.add_argument(
-                "--assign_fill", type=inkex.Boolean, default=True,
-                help="Assign a fill color to the markers")
-        self.arg_parser.add_argument(
-                "-f", "--fill_color", type=inkex.Color, default=inkex.Color(1364325887),
-                help="Choose a custom fill color")
-        self.arg_parser.add_argument(
-                "--assign_stroke", type=inkex.Boolean, dest="assign_stroke", default=True,
-                help="Assign a stroke color to the markers")
-        self.arg_parser.add_argument(
-                "-s", "--stroke_color", type=inkex.Color, default=inkex.Color(1364325887),
-                help="Choose a custom fill color")
-        self.arg_parser.add_argument(
-                "--tab", type=str, dest="tab", default='"custom"',
-                help="The selected UI-tab when OK was pressed")
-        self.arg_parser.add_argument(
-                "--colortab", type=str, dest="colortab",
-                help="The selected custom color tab when OK was pressed")
+    """Add marker stroke to outline markers on selected objects."""
+    def add_arguments(self, pars):
+        pars.add_argument("-m", "--modify", type=inkex.Boolean, default=False,
+                          help="Do not create a copy, modify the markers")
+        pars.add_argument("-t", "--type", dest="fill_type", default="stroke",
+                          help="Replace the markers' fill with the object stroke or fill color")
+        pars.add_argument("-a", "--alpha", type=inkex.Boolean, dest="assign_alpha", default=True,
+                          help="Assign the object fill and stroke alpha to the markers")
+        pars.add_argument("-i", "--invert", type=inkex.Boolean, default=False,
+                          help="Invert fill and stroke colors")
+        pars.add_argument("--assign_fill", type=inkex.Boolean, default=True,
+                          help="Assign a fill color to the markers")
+        pars.add_argument("-f", "--fill_color", type=inkex.Color, default=inkex.Color(1364325887),
+                          help="Choose a custom fill color")
+        pars.add_argument("--assign_stroke", type=inkex.Boolean, default=True,
+                          help="Assign a stroke color to the markers")
+        pars.add_argument("-s", "--stroke_color", type=inkex.Color, default=inkex.Color(1364325887),
+                          help="Choose a custom fill color")
+        pars.add_argument("--tab", type=self.arg_method('method'), default=self.method_custom,
+                          help="The selected UI-tab when OK was pressed")
+        pars.add_argument("--colortab", help="The selected custom color tab when OK was pressed")
+
+    def method_custom(self, _):
+        """Choose custom colors"""
+        fill = self.options.fill_color if self.options.assign_fill else None
+        stroke = self.options.stroke_color if self.options.assign_stroke else None
+        return fill, stroke
+
+    def method_object(self, style):
+        """Use object colors"""
+        fill = style.get_color('fill')
+        stroke = style.get_color('stroke')
+        if self.options.fill_type == "solid":
+            fill = stroke
+        elif self.options.invert:
+            fill, stroke = stroke, fill
+
+        if not self.options.assign_alpha:
+            # Remove alpha values
+            fill = fill.to_rgb()
+            stroke = stroke.to_rgb()
+
+        return fill, stroke
 
     def effect(self):
-        defs = self.svg.defs
+        for node in self.svg.selected.values():
+            fill, stroke = self.options.tab(node.style)
 
-        for id, node in self.svg.selected.items():
-            mprops = ['marker', 'marker-start', 'marker-mid', 'marker-end']
-            try:
-                style = dict(inkex.Style.parse_str(node.get('style')))
-            except:
-                inkex.errormsg(_("No style attribute found for id: %s") % id)
-                continue
+            for attr in MARKERS:
+                if not node.style.get(attr, '').startswith('url(#'):
+                    continue
 
-            # Use object colors
-            if self.options.tab == '"object"':
-                temp_stroke = style.get('stroke', '#000000')
-                temp_fill = style.get('fill', '#000000')
-                if self.options.invert:
-                    fill = temp_stroke
-                    stroke = temp_fill
-                else:
-                    fill = temp_fill
-                    stroke = temp_stroke
-                if self.options.assign_alpha:
-                    temp_stroke_opacity = style.get('stroke-opacity', '1')
-                    temp_fill_opacity = style.get('fill-opacity', '1')
-                    if self.options.invert:
-                        fill_opacity = temp_stroke_opacity
-                        stroke_opacity = temp_fill_opacity
-                    else:
-                        fill_opacity = temp_fill_opacity
-                        stroke_opacity = temp_stroke_opacity
-                if self.options.fill_type == "solid":
-                    fill = stroke
-                    if self.options.assign_alpha:
-                        fill_opacity = stroke_opacity
-            # Choose custom colors
-            elif self.options.tab == '"custom"':
-                fill = str(self.options.fill_color.to_rgb())
-                fill_opacity = self.options.fill_color.alpha
-                stroke = str(self.options.stroke_color.to_rgb())
-                stroke_opacity = self.options.stroke_color.alpha
-                if not self.options.assign_fill:
-                    fill = "none"
-                if not self.options.assign_stroke:
-                    stroke = "none"
+                marker_id = node.style[attr][5:-1]
+                marker_node = self.svg.getElement('/svg:svg//svg:marker[@id="%s"]' % marker_id)
 
-            for mprop in mprops:
-                if mprop in style and style[mprop] != 'none' and style[mprop][:5] == 'url(#':
-                    marker_id = style[mprop][5:-1]
-                    try:
-                        old_mnode = self.svg.getElement('/svg:svg//svg:marker[@id="%s"]' % marker_id)
-                        if self.options.modify:
-                            mnode = old_mnode
-                        else:
-                            mnode = copy.deepcopy(old_mnode)
-                    except:
-                        inkex.errormsg(_("unable to locate marker: %s") % marker_id)
-                        continue
+                if marker_node is None:
+                    inkex.errormsg(_("unable to locate marker: %s") % marker_id)
+                    continue
 
-                    if self.options.modify:
-                        new_id = marker_id
-                    else:
-                        new_id = self.svg.get_unique_id(marker_id)
+                if not self.options.modify:
+                    marker_node = copy.deepcopy(marker_node)
+                    self.svg.defs.append(marker_node)
+                    marker_id = self.svg.get_unique_id(marker_id)
 
-                    style[mprop] = "url(#%s)" % new_id
-                    mnode.set('id', new_id)
-                    mnode.set(inkex.addNS('stockid', 'inkscape'), new_id)
-                    if not self.options.modify:
-                        defs.append(mnode)
+                node.style[attr] = "url(#%s)" % marker_id
+                marker_node.set('id', marker_id)
+                marker_node.set('inkscape:stockid', marker_id)
 
-                    children = mnode.xpath('.//*[@style]')
-                    for child in children:
-                        cstyle = dict(inkex.Style.parse_str(child.get('style')))
-                        if not ('stroke' in cstyle and self.options.tab == '"object"' and cstyle['stroke'] == 'none' and self.options.fill_type == "filled"):
-                            cstyle['stroke'] = stroke
-                            if 'stroke_opacity' in locals():
-                                cstyle['stroke-opacity'] = stroke_opacity
-                        if not ('fill' in cstyle and self.options.tab == '"object"' and cstyle['fill'] == 'none' and self.options.fill_type == "solid"):
-                            cstyle['fill'] = fill
-                            if 'fill_opacity' in locals():
-                                cstyle['fill-opacity'] = fill_opacity
-                        child.set('style', str(inkex.Style(cstyle)))
-            node.set('style', str(inkex.Style(style)))
-
+                children = marker_node.xpath('.//*[@style]')
+                for child in children:
+                    if stroke is not None and self.options.fill_type != "filled":
+                        child.style.set_color(stroke, 'stroke')
+                    if fill is not None and self.options.fill_type != "solid":
+                        child.style.set_color(fill, 'fill')
 
 if __name__ == '__main__':
     MarkerStrokePaintEffect().run()
--- tests/test_markers_strokepaint.py
+++ tests/test_markers_strokepaint.py
@@ -4,25 +4,24 @@
 # Revision history:
 #  * 2012-01-27 (jazzynico): checks defaulf parameters and file handling.
 #
-import inkex
 from markers_strokepaint import MarkerStrokePaintEffect
-from inkex.tester import InkscapeExtensionTestMixin, TestCase
+from inkex.tester import ComparisonMixin, TestCase
 
 
-class MarkerStrokePaintBasicTest(InkscapeExtensionTestMixin, TestCase):
+class MarkerStrokePaintBasicTest(ComparisonMixin, TestCase):
     effect_class = MarkerStrokePaintEffect
-
-    def test_empty_defs(self):
-        e = MarkerStrokePaintEffect()
-        e.run([self.data_file('svg', 'minimal-blank.svg')])
-        self.assertTrue(0 < len(e.svg.xpath('//svg:defs')))
+    compare_file = 'svg/markers.svg'
+    comparisons = [
+        ('--tab="object"', '--id=dimension'),
+        ('--tab="custom"', '--id=dimension'),
+    ]
 
     def test_basic(self):
         args = ['--id=dimension',
                 self.data_file('svg', 'markers.svg')]
-        e = MarkerStrokePaintEffect()
-        e.run(args)
-        old_markers = e.original_document.getroot().xpath('//svg:defs//svg:marker')
-        new_markers = e.svg.xpath('//svg:defs//svg:marker')
+        eff = MarkerStrokePaintEffect()
+        eff.run(args)
+        old_markers = eff.original_document.getroot().xpath('//svg:defs//svg:marker')
+        new_markers = eff.svg.xpath('//svg:defs//svg:marker')
         self.assertEqual(len(old_markers), 2)
         self.assertEqual(len(new_markers), 4)

4201cdae44ca07997ef3d126760bb038bc622fad
Martin Owens <doctormo@gmail.com>
2019-12-09 20:24:41 -0500

    Refactor dots to path_number_nodes to fix inbox:1047

--- tests/test_dots.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from dots import Dots
-from inkex.tester import ComparisonMixin, InkscapeExtensionTestMixin, TestCase
-from inkex.tester.filters import CompareOrderIndependentStyle
-
-class DotsBasicTest(ComparisonMixin, InkscapeExtensionTestMixin, TestCase):
-    effect_class = Dots
-    comparisons = [('--id=p1', '--id=r3')]
-    compare_filters = [CompareOrderIndependentStyle()]

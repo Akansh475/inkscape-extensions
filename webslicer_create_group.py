b7e8309fcbaaf060df70399034c859f712e23313
Martin Owens <doctormo@gmail.com>
2020-06-01 03:29:38 -0400

    Clean up webslicer create group

--- webslicer_create_group.py
+++ webslicer_create_group.py
@@ -16,12 +16,14 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-from lxml import etree
+"""Create webslicer group"""
 
 import inkex
-from webslicer_effect import WebSlicerMixin, is_empty
+from inkex.localization import inkex_gettext as _
+from webslicer_effect import WebSlicerMixin
 
 class CreateGroup(WebSlicerMixin, inkex.EffectExtension):
+    """Create new webslicer group"""
     def add_arguments(self, pars):
         pars.add_argument("--tab")
         pars.add_argument("--html-id", dest="html_id")
@@ -30,36 +32,25 @@ class CreateGroup(WebSlicerMixin, inkex.EffectExtension):
         pars.add_argument("--height-unity", dest="height_unity")
         pars.add_argument("--bg-color", dest="bg_color")
 
-    def get_base_elements(self):
-        self.layer = self.get_slicer_layer()
-        if is_empty(self.layer):
-            return inkex.errormsg(_('You must create and select some "Slicer rectangles" before trying to group.'))
-        self.layer_descendants = self.get_descendants_in_array(self.layer)
-
-    def get_descendants_in_array(self, el):
-        descendants = el.getchildren()
-        for e in descendants:
-            descendants.extend(self.get_descendants_in_array(e))
-        return descendants
-
     def effect(self):
-        self.get_base_elements()
         if not self.svg.selected:
-            return inkex.errormsg(_('You must to select some "Slicer rectangles" or other "Layout groups".'))
-        for key, node in self.svg.selected.items():
-            if node not in self.layer_descendants:
-                return inkex.errormsg(_('Oops... The element "%s" is not in the Web Slicer layer') % key)
-        g_parent = node.getparent()
-        group = etree.SubElement(g_parent, 'g')
-        desc = etree.SubElement(group, 'desc')
+            raise inkex.AbortExtension(_('You must to select some "Slicer rectangles" '
+                                         'or other "Layout groups".'))
+
+        base_elements = self.get_slicer_layer().descendants()
+        for key, node in self.svg.selected.id_dict().items():
+            if node not in base_elements:
+                raise inkex.AbortExtension(_(f'The element "{key}" is not in the Web Slicer layer'))
+            g_parent = node.getparent()
+
+        group = g_parent.add(inkex.Group())
+        desc = group.add(inkex.Desc())
         desc.text = self.get_conf_text_from_list(
-                ['html_id', 'html_class',
-                 'width_unity', 'height_unity',
-                 'bg_color'])
+            ['html_id', 'html_class', 'width_unity', 'height_unity', 'bg_color'])
 
         for node in self.svg.selected.values():
             group.insert(1, node)
 
 
 if __name__ == '__main__':
-    WebSlicer_CreateGroup().run()
+    CreateGroup().run()

1d540deb7b15900ecc1fb073aa124460fa8945ff
Martin Owens <doctormo@gmail.com>
2020-08-27 16:01:13 -0400

    Add virtualenv function to location pip

--- inkex/inkscape_env.py
+++ inkex/inkscape_env.py
@@ -26,21 +26,25 @@ Always import *before* anything else.
 import os
 import sys
 
+def get_bin(fname):
+    """Get a virtualenv binary for execution, returns full filename"""
+    for path in sys.path:
+        for script in [fname, os.path.join('bin', fname)]:
+            result = os.path.abspath(os.path.join(path, script))
+            if os.path.isfile(result):
+                return result
+    return None
+
 def activate_virtualenv():
     """ 
     The python that inkscape uses and the python installed into the virtualenv
     are different pythons with different libs. To give access to dependencies
     that are installed within the virtualenv, we activate the available venv.
     """
-    for path in sys.path:
-        for script in [
-                'activate_this.py',
-                os.path.join('bin', 'activate_this.py'),
-                ]:
-            activate_this = os.path.join(path, script)
-            if os.path.isfile(activate_this):
-                with open(activate_this, 'r') as fhl:
-                    exec(fhl.read(), dict(__file__=activate_this))
-                return
+    activate_this = get_bin('activate_this.py')
+    if os.path.isfile(activate_this):
+        with open(activate_this, 'r') as fhl:
+            exec(fhl.read(), dict(__file__=activate_this))
+        return
 
 activate_virtualenv()

5a419da9a1f95aad74fbfc1a963a3adb5b24474b
Martin Owens <doctormo@gmail.com>
2020-11-12 16:34:27 -0500

    Fix inx test for tiff export

--- raster_output_tiff.py
+++ raster_output_tiff.py
@@ -9,6 +9,7 @@ class TiffOutput(inkex.RasterOutputExtension):
     def add_arguments(self, pars):
         pars.add_argument('--tab')
         pars.add_argument('--compression', default=None)
+        pars.add_argument('--quality', type=int, default=95)
         pars.add_argument('--progressive', type=inkex.Boolean, default=False)
 
     def save(self, stream):

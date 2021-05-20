d5c5ca0a4c048875170090e465fe29fb4547ae47
Jonathan Neuhauser <jonathan.neuhauser@outlook.com>
2021-05-21 00:22:27 +0200

    Respect parent transforms for "measure path" and "number nodes"

--- tests/test_measure.py
+++ tests/test_measure.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from inkex.utils import filename_arg
 from measure import MeasureLength
 from inkex.tester import ComparisonMixin, TestCase
 from inkex.tester.filters import CompareNumericFuzzy
@@ -15,4 +16,14 @@ class LengthBasicTest(ComparisonMixin, TestCase):
         ('--method=presets', '--presetFormat=FT_bbox', '--id=p2'),
         ('--type=area', '--id=p1'),
         ('--type=cofm', '--id=c3'),
+    ]
+
+class LengthComplexTransformTest(ComparisonMixin, TestCase):
+    effect_class = MeasureLength
+    compare_filters = [CompareNumericFuzzy()]
+    compare_file = "svg/complextransform.test.svg"
+    comparisons = [
+        ('--method=presets', '--presetFormat=TaP_start', '--id=D'),
+        ('--method=presets', '--presetFormat=FT_start', '--id=D'),
+        ('--type=cofm', '--id=D'),
     ]

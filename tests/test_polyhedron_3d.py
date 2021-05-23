172cec0a312e2dbce1f8ae9c844f4e1c400c6748
Nathan Lee <2431820-nathanal@users.noreply.gitlab.com>
2021-05-23 17:13:14 +1000

    Fix 3D Polyhedron rendering from custom file
    Restores 0.92.5 behavior
    Fix https://gitlab.com/inkscape/extensions/-/issues/342

--- tests/test_polyhedron_3d.py
+++ tests/test_polyhedron_3d.py
@@ -9,6 +9,7 @@ class Poly3DBasicTest(ComparisonMixin, TestCase):
         ('--show=fce', '--obj=cube', '--r1_ax=y', '--r1_ang=45', '--z_sort=cent'),
         ('--show=fce', '--obj=cube', '--r1_ax=z', '--r1_ang=45', '--z_sort=max'),
         ('--show=edg', '--obj=oct', '--r1_ax=z', '--r1_ang=45', '--th=4'),
-        ('--show=vtx', '--obj=methane',),
+        ('--show=vtx', '--obj=methane'),
+        ('--show=fce', '--obj=from_file', '--spec_file=great_stel_dodec.obj'),
     ]
     compare_file = 'svg/empty.svg'

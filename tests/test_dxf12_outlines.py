67f0c709ce3c8b9d76c3ad9c57fbc4f47bdd7d97
Jonathan Neuhauser <jonathan.neuhauser@outlook.com>
2021-07-02 22:55:49 +0200

    fix transform order in dxf_outlines

--- tests/test_dxf12_outlines.py
+++ tests/test_dxf12_outlines.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 from dxf12_outlines import DxfTwelve
-from inkex.tester import InkscapeExtensionTestMixin, TestCase
+from inkex.tester import ComparisonMixin, TestCase
 
 
-class TestDXF12OutlinesBasic(InkscapeExtensionTestMixin, TestCase):
+class TestDXF12OutlinesBasic(ComparisonMixin, TestCase):
+    compare_file = ["svg/shapes.svg", "svg/preserved-transforms.svg"]
+    comparisons = [()]
     effect_class = DxfTwelve

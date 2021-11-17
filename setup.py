be84101599b9746caf9077cab6576f99b47f40f2
Daniel Ziegenberg <daniel@ziegenberg.at>
2021-11-17 09:40:23 +0100

    add python 3.10 to CI pipeline and project
    Signed-off-by: Daniel Ziegenberg <daniel@ziegenberg.at>

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
         ],
         packages=['inkex', 'inkex.elements', 'inkex.tester'],
         install_requires=['scour', 'numpy', 'pyserial', 'cssselect'],

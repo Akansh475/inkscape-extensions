e84b0f7afdcece65d767e2f1377e4836005bab80
Jonathan Neuhauser <jonathan.neuhauser@outlook.com>
2022-01-16 17:44:47 +0100

    fix #408

--- tests/test_polyhedron_3d.py
+++ tests/test_polyhedron_3d.py
@@ -12,6 +12,7 @@ class Poly3DBasicTest(ComparisonMixin, TestCase):
         ('--show=fce', '--obj=cube', '--r1_ax=z', '--r1_ang=45', '--z_sort=max', '--cw_wound=True'),
         ('--show=edg', '--obj=oct', '--r1_ax=z', '--r1_ang=45', '--th=4', '--cw_wound=True'),
         ('--show=vtx', '--obj=methane', '--cw_wound=True'),
+        ('--show=edg', '--obj=methane', '--cw_wound=True'),
         ('--show=fce', '--obj=from_file', '--spec_file=great_stel_dodec.obj', '--cw_wound=True'),
     ]
     compare_file = 'svg/empty.svg'

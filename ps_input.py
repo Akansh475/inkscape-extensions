0948765ab2bdad4408d6de7c0c4500bcf3b12c02
Jonathan Neuhauser <jonathan.neuhauser@outlook.com>
2022-01-31 19:23:35 +0000

    Ps2pdf: allow special chars on Windows

--- ps_input.py
+++ ps_input.py
@@ -21,8 +21,11 @@
 Simple wrapper around ps2pdf
 """
 
+import sys
+import os
+
 import inkex
-from inkex.command import call
+from inkex.command import call, which
 
 class PostscriptInput(inkex.CallExtension):
     """Load Postscript/EPS Files by calling ps2pdf program"""
@@ -35,7 +38,27 @@ class PostscriptInput(inkex.CallExtension):
 
     def call(self, input_file, output_file):
         crop = '-dEPSCrop' if self.options.crop else None
-        call('ps2pdf', crop, input_file, output_file)
+        if sys.platform == "win32":
+            params = ['-q', '-P-', '-dSAFER', '-dNOPAUSE', '-dBATCH', '-sDEVICE#pdfwrite',
+            '-dCompatibilityLevel#1.4', crop, "-sOutputFile#" + output_file, input_file]
+            gs_execs = ["gswin64c", "gswin32c"]
+            gs_exec = None
+            for executable in gs_execs:
+                try:
+                    which(executable)
+                    gs_exec = executable
+                except:
+                    pass
+            if gs_exec is None:
+                if "PYTEST_CURRENT_TEST" in os.environ:
+                    gs_exec = "gswin64c" # In CI, we have neither available, 
+                                         # but there are mock files for the 64 bit version
+                else:
+                    raise inkex.AbortExtension()
+            call(gs_exec, *params)
+        else:
+            call("ps2pdf", crop, input_file, output_file)
+
 
 if __name__ == '__main__':
     PostscriptInput().run()

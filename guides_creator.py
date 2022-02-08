57fe6b958412d923085dc417ca613d9dc4a32de0
Jonathan Neuhauser <jonathan.neuhauser@outlook.com>
2022-02-08 12:25:51 +0100

    refactor guides creator

--- guides_creator.py
+++ guides_creator.py
@@ -32,6 +32,21 @@ from math import cos, sin, sqrt
 import inkex
 from inkex import Guide
 
+from inkex.localization import inkex_gettext as _
+
+
+class GuidesOpts:
+    """Value storage for Guides Creator"""
+
+    # pylint: disable=too-few-public-methods
+    def __init__(self, svg) -> None:
+        self.width = svg.viewbox_width
+        self.height = svg.viewbox_height
+
+        # getting edges coordinates
+        self.h_orientation = (0, round(self.width, 4))
+        self.v_orientation = (round(self.height, 4), 0)
+
 
 class GuidesCreator(inkex.EffectExtension):
     """Create a set of guides based on the given options"""
@@ -65,23 +80,30 @@ class GuidesCreator(inkex.EffectExtension):
         pars.add_argument(
             "--lr", type=inkex.Boolean, default=False, help="Lower right corner"
         )
-        pars.add_argument("--margins_preset", default="custom", help="Margins preset")
+        pars.add_argument(
+            "--margins_preset",
+            default="custom",
+            choices=["custom", "book_left", "book_right"],
+            help="Margins preset",
+        )
         pars.add_argument("--vert", type=int, default=0, help="Vert subdivisions")
         pars.add_argument("--horz", type=int, default=0, help="Horz subdivisions")
-        pars.add_argument("--header_margin", default="10", help="Header margin")
-        pars.add_argument("--footer_margin", default="10", help="Footer margin")
-        pars.add_argument("--left_margin", default="10", help="Left margin")
-        pars.add_argument("--right_margin", default="10", help="Right margin")
+        pars.add_argument(
+            "--header_margin", type=int, default="10", help="Header margin"
+        )
+        pars.add_argument(
+            "--footer_margin", type=int, default="10", help="Footer margin"
+        )
+        pars.add_argument("--left_margin", type=int, default="10", help="Left margin")
+        pars.add_argument("--right_margin", type=int, default="10", help="Right margin")
         pars.add_argument("--delete", type=inkex.Boolean, help="Delete existing guides")
 
-    def effect(self):
-        # getting the width and height attributes of the canvas
-        self.width = float(self.svg.viewbox_width)
-        self.height = float(self.svg.viewbox_height)
+    def __init__(self):
+        super().__init__()
+        self.opts: GuidesOpts = None
 
-        # getting edges coordinates
-        self.h_orientation = "0," + str(round(self.width, 4))
-        self.v_orientation = str(round(self.height, 4)) + ",0"
+    def effect(self):
+        self.opts = GuidesOpts(self.svg)
 
         if self.options.delete:
             for guide in self.svg.namedview.get_guides():
@@ -106,28 +128,17 @@ class GuidesCreator(inkex.EffectExtension):
         elif preset == "golden":
             gold = (1 + sqrt(5)) / 2
 
-            # horizontal golden guides
-            position1 = "0," + str(self.height / gold)
-            position2 = "0," + str(self.height - (self.height / gold))
-
-            self.draw_guide(position1, self.h_orientation)
-            self.draw_guide(position2, self.h_orientation)
-
-            # vertical golden guides
-            position1 = str(self.width / gold) + ",0"
-            position2 = str(self.width - (self.width / gold)) + ",0"
-
-            self.draw_guide(position1, self.v_orientation)
-            self.draw_guide(position2, self.v_orientation)
+            for fraction, index in zip([1 / gold, 1 - 1 / gold] * 2, [1, 1, 0, 0]):
+                position = fraction * (self.opts.width, self.opts.height)[index]
+                self.draw_guide(
+                    (0, position) if index == 1 else (position, 0),
+                    (self.opts.v_orientation, self.opts.h_orientation)[index],
+                )
 
             if from_edges:
-                # horizontal borders
-                self.draw_guide("0," + str(self.height), self.h_orientation)
-                self.draw_guide(str(self.height) + ",0", self.h_orientation)
 
-                # vertical borders
-                self.draw_guide("0," + str(self.width), self.v_orientation)
-                self.draw_guide(str(self.width) + ",0", self.v_orientation)
+                self.draw_guides(1, True, vert=False)
+                self.draw_guides(1, True, vert=True)
 
         elif ";" in preset:
             v_division = int(preset.split(";")[0])
@@ -135,160 +146,125 @@ class GuidesCreator(inkex.EffectExtension):
             self.draw_guides(v_division, from_edges, vert=True)
             self.draw_guides(h_division, from_edges, vert=False)
         else:
-            raise inkex.AbortExtension("Unknown guide guide preset: {}".format(preset))
+            raise inkex.AbortExtension(_("Unknown guide preset: {}").format(preset))
 
     def generate_diagonal_guides(self):
         """Generate diagonal guides"""
         # Dimentions
         left, bottom = (0, 0)
-        right, top = (self.width, self.height)
+        right, top = (self.opts.width, self.opts.height)
 
         # Diagonal angle
         angle = 45
 
-        if self.options.ul:
-            ul_corner = str(top) + "," + str(left)
-            from_ul_to_lr = str(cos(angle)) + "," + str(cos(angle))
-            self.draw_guide(ul_corner, from_ul_to_lr)
+        corner_guides = {
+            "ul": ((top, left), (cos(angle), cos(angle))),
+            "ur": ((right, top), (-sin(angle), sin(angle))),
+            "ll": ((bottom, left), (-cos(angle), cos(angle))),
+            "lr": ((bottom, right), (-sin(angle), -sin(angle))),
+        }
 
-        if self.options.ur:
-            ur_corner = str(right) + "," + str(top)
-            from_ur_to_ll = str(-sin(angle)) + "," + str(sin(angle))
-            self.draw_guide(ur_corner, from_ur_to_ll)
-
-        if self.options.ll:
-            ll_corner = str(bottom) + "," + str(left)
-            from_ll_to_ur = str(-cos(angle)) + "," + str(cos(angle))
-            self.draw_guide(ll_corner, from_ll_to_ur)
-
-        if self.options.lr:
-            lr_corner = str(bottom) + "," + str(right)
-            from_lr_to_ul = str(-sin(angle)) + "," + str(-sin(angle))
-            self.draw_guide(lr_corner, from_lr_to_ul)
+        for key, (position, orientation) in corner_guides.items():
+            if getattr(self.options, key):
+                self.draw_guide(position, orientation)
 
     def generate_margins(self):
         """Generate margin guides"""
-        header_margin = int(self.options.header_margin)
-        footer_margin = int(self.options.footer_margin)
-        left_margin = int(self.options.left_margin)
-        right_margin = int(self.options.right_margin)
-        h_subdiv = int(self.options.horz)
-        v_subdiv = int(self.options.vert)
 
         if self.options.start_from_edges:
             # horizontal borders
-            self.draw_guide("0," + str(self.height), self.h_orientation)
-            self.draw_guide(str(self.height) + ",0", self.h_orientation)
+            self.draw_guide((0, self.opts.height), self.opts.h_orientation)
+            self.draw_guide((self.opts.height, 0), self.opts.h_orientation)
 
             # vertical borders
-            self.draw_guide("0," + str(self.width), self.v_orientation)
-            self.draw_guide(str(self.width) + ",0", self.v_orientation)
+            self.draw_guide((0, self.opts.width), self.opts.v_orientation)
+            self.draw_guide((self.opts.width, 0), self.opts.v_orientation)
 
         if self.options.margins_preset == "custom":
-            y_header = self.height
-            y_footer = 0
-            x_left = 0
-            x_right = self.width
+            margins = [
+                (i / j if int(j) != 0 else None)
+                for i, j in zip(
+                    (
+                        self.opts.height * (self.options.header_margin - 1),  # header
+                        self.opts.height,  # footer
+                        self.opts.width,  # left
+                        self.opts.width * (self.options.right_margin - 1),  # right
+                    ),
+                    (
+                        self.options.header_margin,
+                        self.options.footer_margin,
+                        self.options.left_margin,
+                        self.options.right_margin,
+                    ),
+                )
+            ]
 
-            if header_margin != 0:
-                y_header = (self.height / header_margin) * (header_margin - 1)
-                self.draw_guide("0," + str(y_header), self.h_orientation)
+        book_options = {
+            "book_left": (8 / 9, 2 / 9, 2 / 9, 8 / 9),
+            "book_right": (8 / 9, 2 / 9, 1 / 9, 7 / 9),
+        }
+        if self.options.margins_preset in book_options:
+            margins = [
+                i * j
+                for i, j in zip(
+                    book_options[self.options.margins_preset],
+                    2 * [self.opts.height] + 2 * [self.opts.width],
+                )
+            ]
 
-            if footer_margin != 0:
-                y_footer = self.height / footer_margin
-                self.draw_guide("0," + str(y_footer), self.h_orientation)
+        y_header, y_footer, x_left, x_right = [
+            i or j for i, j in zip(margins, [self.opts.height, 0, 0, self.opts.width])
+        ]
 
-            if left_margin != 0:
-                x_left = self.width / left_margin
-                self.draw_guide(str(x_left) + ",0", self.v_orientation)
-
-            if right_margin != 0:
-                x_right = (self.width / right_margin) * (right_margin - 1)
-                self.draw_guide(str(x_right) + ",0", self.v_orientation)
-
-        elif self.options.margins_preset == "book_left":
-            # 1/9th header
-            y_header = (self.height / 9) * 8
-            self.draw_guide("0," + str(y_header), self.h_orientation)
-
-            # 2/9th footer
-            y_footer = (self.height / 9) * 2
-            self.draw_guide("0," + str(y_footer), self.h_orientation)
-
-            # 2/9th left margin
-            x_left = (self.width / 9) * 2
-            self.draw_guide(str(x_left) + ",0", self.v_orientation)
-
-            # 1/9th right margin
-            x_right = (self.width / 9) * 8
-            self.draw_guide(str(x_right) + ",0", self.v_orientation)
-
-        elif self.options.margins_preset == "book_right":
-            # 1/9th header
-            y_header = (self.height / 9) * 8
-            self.draw_guide("0," + str(y_header), self.h_orientation)
-
-            # 2/9th footer
-            y_footer = (self.height / 9) * 2
-            self.draw_guide("0," + str(y_footer), self.h_orientation)
-
-            # 2/9th left margin
-            x_left = self.width / 9
-            self.draw_guide(str(x_left) + ",0", self.v_orientation)
-
-            # 1/9th right margin
-            x_right = (self.width / 9) * 7
-            self.draw_guide(str(x_right) + ",0", self.v_orientation)
+        for length, position in zip(margins, [1, 1, 0, 0]):
+            if length is None:
+                continue
+            self.draw_guide(
+                (length, 0) if position == 0 else (0, length),
+                (self.opts.v_orientation, self.opts.h_orientation)[position],
+            )
 
         # setting up properties of the rectangle created between guides
         rectangle_height = y_header - y_footer
         rectangle_width = x_right - x_left
 
-        if h_subdiv != 0:
-            begin_from = y_footer
-            # creating horizontal guides
-            self._draw_guides(
-                (rectangle_width, rectangle_height),
-                h_subdiv,
-                edges=0,
-                shift=begin_from,
-                vert=False,
-            )
-
-        if v_subdiv != 0:
-            begin_from = x_left
-            # creating vertical guides
-            self._draw_guides(
-                (rectangle_width, rectangle_height),
-                v_subdiv,
-                edges=0,
-                shift=begin_from,
-                vert=True,
-            )
+        for subdiv, vert, begin_from in zip(
+            (self.options.horz, self.options.vert), (False, True), (y_footer, x_left)
+        ):
+            if subdiv != 0:
+                self._draw_guides(
+                    (rectangle_width, rectangle_height),
+                    subdiv,
+                    edges=0,
+                    shift=begin_from,
+                    vert=vert,
+                )
 
     def draw_guides(self, division, edges, vert=False):
         """Draw a vertical or horizontal lines"""
-        return self._draw_guides((self.width, self.height), division, edges, vert=vert)
+        return self._draw_guides(
+            (self.opts.width, self.opts.height), division, edges, vert=vert
+        )
 
     def _draw_guides(self, vector, division, edges, shift=0, vert=False):
         if division <= 0:
             return
 
         # Vert controls both ort template and vector calculation
-        ort = "{},0" if vert else "0,{}"
+        def ort(x):
+            return (x, 0) if vert else (0, x)
+
         var = int(bool(edges))
         for x in range(0, division - 1 + 2 * var):
             div = vector[not bool(vert)] / division
-            position = str(round(div + (x - var) * div + shift, 4))
-            orientation = str(round(vector[bool(vert)], 4))
-            self.draw_guide(ort.format(position), ort.format(orientation))
+            position = round(div + (x - var) * div + shift, 4)
+            orientation = round(vector[bool(vert)], 4)
+            self.draw_guide(ort(position), ort(orientation))
 
     def draw_guide(self, position, orientation):
         """Create a guide directly into the namedview"""
         if isinstance(position, tuple):
             x, y = position
-        elif isinstance(position, str):
-            x, y = position.split(",")
         self.svg.namedview.add(Guide().move_to(float(x), float(y), orientation))
 
 

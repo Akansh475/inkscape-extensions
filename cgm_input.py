db632c37180654fc93001b8e31b82b00d9e2a52f
Ximo Planells Lerma <ximo@planells.ie>
2026-03-12 22:40:11 +0000

    Fix undefined variable 'color' in cgm_input.py

--- cgm_input.py
+++ cgm_input.py
@@ -191,7 +191,7 @@ class CGMConverter(metaclass=CGMConverterMeta):
         if isinstance(colour, cgm_enums.DirectColour):
             if colour.colour_model == cgm_enums.ColourModelEnum.RGB:
                 return ColorRGB([colour.v1, colour.v2, colour.v3])
-            elif color.colour_model == cgm_enums.ColorModelEnum.CMYK:
+            elif colour.colour_model == cgm_enums.ColorModelEnum.CMYK:
                 return ColorDeviceCMYK([colour.v1, colour.v2, colour.v3, colour.v4])
             else:
                 inkex.errormsg(f"Color model {colour.colour_model} not implemented")
